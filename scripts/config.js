31bbb7d766ed58e42dbb6e9bd92250e8a09a14d2
mar <mar.nospam@anomy.net>
2020-03-10 18:20:31 +0000

    chore: Ignore all css.map files and "public/css/dev/"

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,7 @@
 const pkg = require('../package.json');
 
 const cssVersion = 'v' + (pkg.version.match(/^\d+\.\d+/) || [''])[0];
-const devVersion = 'canary';
+const devVersion = 'dev';
 const distFolder = 'public/css/';
 
 module.exports = {
