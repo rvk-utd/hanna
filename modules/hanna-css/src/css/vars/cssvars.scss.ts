9d54891e050ef5087911fa66e28551dc477b40f3
Már Örlygsson <mar.nospam@anomy.net>
2022-05-16 15:51:59 +0000

    style: Use direct export

--- modules/hanna-css/src/css/vars/cssvars.scss.ts
+++ modules/hanna-css/src/css/vars/cssvars.scss.ts
@@ -1,8 +1,5 @@
 import { hannaVars } from '../../lib/hannavars';
 
-const scssCssVariables =
-  Object.values(hannaVars)
-    .map((printer) => `$var${printer.cssName}: ${printer};`)
-    .join('\n') + '\n';
-
-export default scssCssVariables;
+export default Object.values(hannaVars)
+  .map((printer) => `$var${printer.cssName}: ${printer};`)
+  .join('\n') + '\n';
--- modules/hanna-css/src/css/vars/icons.scss.ts
+++ modules/hanna-css/src/css/vars/icons.scss.ts
@@ -1,11 +1,8 @@
 import iconfonttokens from '../../lib/iconfonttokens';
 
-const iconVariables =
-  Object.entries(iconfonttokens)
-    .map(
-      ([name, unicode]) =>
-        `$icons-${name.replace(/^icon__/, '').replace(/_/g, '-')}: '${unicode}';`
-    )
-    .join('\n') + '\n';
-
-export default iconVariables;
+export default Object.entries(iconfonttokens)
+  .map(
+    ([name, unicode]) =>
+      `$icons-${name.replace(/^icon__/, '').replace(/_/g, '-')}: '${unicode}';`
+  )
+  .join('\n') + '\n';
