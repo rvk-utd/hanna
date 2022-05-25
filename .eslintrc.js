a53eb1c5742f0a71b65d7a1abf81ad85f0012459
Már Örlygsson <mar.nospam@anomy.net>
2022-05-25 16:16:18 +0000

    chore: Remove custom eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = require('@hugsmidjan/hxmstyle')({
   // Place your project-specific additions or overrides here
   // using standard ESLint config syntax...
-
-  rules: {
-    'total-functions/require-strict-mode': 'error',
-  },
   env: {
     node: true,
     browser: true,
