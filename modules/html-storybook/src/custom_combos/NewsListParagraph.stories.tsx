80a2c3317e45b6c6745bc76393bffbe84312a547
Már Örlygsson <mar.nospam@anomy.net>
2022-06-21 09:38:55 +0000

    story: Clean up NewsList paragraph story

--- modules/html-storybook/src/custom_combos/NewsListParagraph.stories.tsx
+++ modules/html-storybook/src/custom_combos/NewsListParagraph.stories.tsx
@@ -6,7 +6,6 @@ import BgBox from '@reykjavik/hanna-react/BgBox';
 import ButtonTertiary from '@reykjavik/hanna-react/ButtonTertiary';
 import Heading from '@reykjavik/hanna-react/Heading';
 import Picture from '@reykjavik/hanna-react/Picture';
-import { boolean } from '@storybook/addon-knobs';
 
 import landscapeImage from '../example_assets/NewsHero__landscape.jpg';
 import { StoryComponent, StoryParameters } from '../storytypes';
@@ -16,115 +15,101 @@ import HiddenTiger from '../utils/HiddenTiger';
 export default {
   title: '_misc/reykjavik-is',
   parameters: {
-    knobs: { disabled: false },
+    // knobs: { disabled: false },
   } as StoryParameters,
 };
 
-const customStyles = css`
-  .NewsListParagraph {
-  }
-  .NewsListParagraph .Heading {
-    margin-top: 0;
-  }
-  .NewsListTopItem {
-    margin-bottom: var(--component-vspace--medium);
-  }
-  @media ${mq.phone_phablet} {
-    .NewsListTopItem .Picture > img {
-      position: relative;
-    }
-  }
-  @media ${mq.tablet_up} {
-    .NewsListTopItem {
-      display: flex;
-      justify-content: space-between;
-    }
-    .NewsListTopItem .Picture {
-      margin-bottom: 0;
-      margin-left: ${hannaVars.grid_margin__neg};
-      width: calc(${hannaVars.grid_5_5} + ${hannaVars.grid_margin});
-      padding-top: 50%;
-      height: auto;
-    }
-    .NewsListTopItem__content {
-      width: ${hannaVars.grid_6_6};
-      align-self: center;
-    }
-  }
-  @media ${mq.netbook_up} {
-    .NewsListTopItem__content {
-      width: ${hannaVars.grid_6};
-    }
-  }
-  @media ${mq.wide} {
-    .NewsListTopItem__content {
-      padding-left: calc(0.5 * ${hannaVars.grid_gutter});
-    }
-  }
-
-  .NewsListTopItem .Heading {
-    margin-bottom: ${hannaVars.space_1};
-  }
-  .NewsListTopItem__summary {
-    font: ${hannaVars.font_bd_l};
-    margin-top: ${hannaVars.space_2};
-    margin-bottom: ${hannaVars.space_2};
-  }
-`;
-
 const imageCards = _imageCards.slice(2).slice(0, 4);
 
-const cssTokens = 'ArticleCards,Heading,Picture,ArticleMeta,ButtonTertiary';
-const bgBoxToken = ',BgBox';
+const cssTokens = 'ArticleCards,ArticleMeta,BgBox,ButtonTertiary,Heading,Picture';
 
-export const NewsListParagraph: StoryComponent = () => {
-  const Container = boolean('Grey background', true) ? BgBox : 'div';
+export const NewsListParagraph: StoryComponent = () => (
+  <>
+    <style>{css`
+      .NewsListParagraph {
+      }
+      .NewsListParagraph .Heading {
+        margin-top: 0;
+      }
+      .NewsListTopItem {
+        margin-bottom: var(--component-vspace--medium);
+      }
+      @media ${mq.phone_phablet} {
+        .NewsListTopItem .Picture > img {
+          position: relative;
+        }
+      }
+      @media ${mq.tablet_up} {
+        .NewsListTopItem {
+          display: flex;
+          justify-content: space-between;
+        }
+        .NewsListTopItem .Picture {
+          margin-bottom: 0;
+          margin-left: ${hannaVars.grid_margin__neg};
+          width: calc(${hannaVars.grid_5_5} + ${hannaVars.grid_margin});
+          padding-top: 50%;
+          height: auto;
+        }
+        .NewsListTopItem__content {
+          width: ${hannaVars.grid_6_6};
+          align-self: center;
+        }
+      }
+      @media ${mq.netbook_up} {
+        .NewsListTopItem__content {
+          width: ${hannaVars.grid_6};
+        }
+      }
+      @media ${mq.wide} {
+        .NewsListTopItem__content {
+          padding-left: calc(0.5 * ${hannaVars.grid_gutter});
+        }
+      }
 
-  let displayedCssTokens = cssTokens;
-  if (Container !== 'div') {
-    displayedCssTokens += bgBoxToken;
-  }
+      .NewsListTopItem .Heading {
+        margin-bottom: ${hannaVars.space_1};
+      }
+      .NewsListTopItem__summary {
+        font: ${hannaVars.font_bd_l};
+        margin-top: ${hannaVars.space_2};
+        margin-bottom: ${hannaVars.space_2};
+      }
+    `}</style>
 
-  return (
-    <>
-      <style>{customStyles}</style>
-      <Container className="NewsListParagraph">
-        <Heading>Fréttir</Heading>
-        <div className="NewsListTopItem">
-          <Picture src={landscapeImage} altText="Some alt text" />
-          <div className="NewsListTopItem__content">
-            <Heading Tag="h3" size="large">
-              <a href="">Útivistarsvæðin í borginni iða af lífi</a>
-            </Heading>
-            <ArticleMeta items={[{ label: 'Mánudagur , 13. apríl 2022' }]} />
-            <div className="NewsListTopItem__summary">
-              Reykvíkingar eru heppnir að geta valið milli margra spennandi útivistarsvæða
-              þar sem er hægt að viðra sig og næra líkama og sál. Þessi svæði eru
-              sérstaklega mikilvæg nú á tímum samkomu- banns og aflýstra viðburða.
-              Náttúran er enn opin og á útivistar- svæðum er auðvelt að hlýða Víði og
-              virða tveggja metra regluna en á sama tíma finna fyrir ákveðinni nálægð við
-              annað fólk.
-            </div>
-            <ButtonTertiary href="" aria-label="Útivistarsvæðin í borginni iða af lífi">
-              Lesa meira
-            </ButtonTertiary>
+    <BgBox className="NewsListParagraph">
+      <Heading>Fréttir</Heading>
+      <div className="NewsListTopItem">
+        <Picture src={landscapeImage} altText="Some alt text" />
+        <div className="NewsListTopItem__content">
+          <Heading Tag="h3" size="large">
+            <a href="">Útivistarsvæðin í borginni iða af lífi</a>
+          </Heading>
+          <ArticleMeta items={[{ label: 'Mánudagur , 13. apríl 2022' }]} />
+          <div className="NewsListTopItem__summary">
+            Reykvíkingar eru heppnir að geta valið milli margra spennandi útivistarsvæða
+            þar sem er hægt að viðra sig og næra líkama og sál. Þessi svæði eru
+            sérstaklega mikilvæg nú á tímum samkomu- banns og aflýstra viðburða. Náttúran
+            er enn opin og á útivistar- svæðum er auðvelt að hlýða Víði og virða tveggja
+            metra regluna en á sama tíma finna fyrir ákveðinni nálægð við annað fólk.
           </div>
+          <ButtonTertiary href="" aria-label="Útivistarsvæðin í borginni iða af lífi">
+            Lesa meira
+          </ButtonTertiary>
         </div>
-        <ArticleCards cards={imageCards} />
-      </Container>
+      </div>
+      <ArticleCards cards={imageCards} />
+    </BgBox>
 
-      <HiddenTiger>
-        CSS tokens:{' '}
-        <a href={getCssBundleUrl(displayedCssTokens)}>
-          {displayedCssTokens.replace(',', ', ')}
-        </a>
-      </HiddenTiger>
-    </>
-  );
-};
+    {/* Content hidden from the HTML source rendering tab */}
+    <HiddenTiger>
+      CSS tokens: <a href={getCssBundleUrl(cssTokens)}>{cssTokens.replace(',', ', ')}</a>
+    </HiddenTiger>
+  </>
+);
 
 NewsListParagraph.story = {
   parameters: {
-    css: { tokens: cssTokens + bgBoxToken },
+    css: { tokens: cssTokens },
   },
 };
