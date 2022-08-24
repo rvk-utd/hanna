e4618fd5a21bf896a003cd91762de13481a99caf
Már Örlygsson <mar.nospam@anomy.net>
2022-08-24 17:21:58 +0000

    feat: Add props `slots` to VRT `Minimal` layout componenbt

--- modules/visual-tests/src/layout/Minimal.tsx
+++ modules/visual-tests/src/layout/Minimal.tsx
@@ -1,18 +1,26 @@
 import type { ReactNode } from 'react';
 import React from 'react';
 
+const roles = {
+  main: 'main',
+  nav: 'navigation',
+  footer: 'complementary',
+};
+
 export type MinimalProps = {
+  slot?: 'main' | 'nav' | 'footer';
   children: ReactNode;
 };
 
 /**
- * Minimal, no-frills, no-chrome, "__main"-only replacement for the `Layout` component,
+ * Minimal, no-frills, no-chrome, pseudo-replacement for the `Layout` component,
  * for use by visual testing pages.
  */
 export const Minimal = (props: MinimalProps) => {
+  const slot = props.slot || 'main';
   return (
     <div className="Layout">
-      <div className="Layout__main" role="main">
+      <div className={'Layout__' + slot} role={roles[slot]}>
         {props.children}
       </div>
     </div>
