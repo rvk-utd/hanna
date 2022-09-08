001d5315a2f3aea77ed7f05bddfe8d1dfa711e00
Már Örlygsson <mar.nospam@anomy.net>
2022-09-08 14:59:11 +0000

    tests: Add dummy lorem ipsum text

--- modules/visual-tests/src/dummyData.ts
+++ modules/visual-tests/src/dummyData.ts
@@ -9,3 +9,21 @@ export const photo = {
   square: { src: '/media/photo-square.png', altText: 'Photo, square' },
   portrait: { src: '/media/photo-portrait.png', altText: 'Photo, portrait' },
 };
+
+export const lorem = {
+  tiny: 'Leebur deroor iehroom, bork bork börk! Enim ad minim chokolat moose.',
+  short:
+    'Yöva gäta stahrt sömwaer. Ut enim ad minim venäm, letsi börk ifder ' +
+    'svensk og latin makinen dehr graek.',
+  medium:
+    'Vender gests kämmen vewänthärdis wehrdsen onderpasje moose. Ut enim ' +
+    'ad minim veniam, letsi ifder svensk og latin makinen dehr graek. ' +
+    'Yöva gäta stahrt sömwaer. Nawei raitinen de wehbsyte.',
+  long:
+    'Lörem ipsum dolor sit amet, dedeeshka duu. Leebur deroor iehroom, bork ' +
+    'bork börk! Ut enim ad minim veniam, letsi ifder svensk og latin ' +
+    'makinen dehr graek. Ut enim ad minim veniam, letsi ifder svensk og ' +
+    'latin makinen dehr graek. Nawei raitinen de wehbsyte. Ut enim ad minim ' +
+    'veniam, letsi ifder svensk og latin makinen dehr graek. Ut enim ad ' +
+    'minim veniam, letsi ifder svensk og latin makinen dehr graek.',
+};
