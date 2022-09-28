10e4da0a4094e1f650fde9a1f68d70f6e9e6cf63
Már Örlygsson <mar.nospam@anomy.net>
2022-09-28 12:15:34 +0000

    tests: Sandwich all <Minimal/> tests between two dummy blocks by default

--- modules/visual-tests/src/layout/Minimal.tsx
+++ modules/visual-tests/src/layout/Minimal.tsx
@@ -2,6 +2,8 @@ import type { ReactNode } from 'react';
 import React from 'react';
 import { useScrollbarWidthCSSVar } from '@reykjavik/hanna-react/utils';
 
+import { DummyBlock } from './DummyBlock';
+
 const roles = {
   main: 'main',
   nav: 'navigation',
@@ -10,6 +12,7 @@ const roles = {
 
 export type MinimalProps = {
   slot?: 'main' | 'nav' | 'footer';
+  bare?: boolean;
   children: ReactNode;
 };
 
@@ -19,11 +22,18 @@ export type MinimalProps = {
  */
 export const Minimal = (props: MinimalProps) => {
   useScrollbarWidthCSSVar();
+  const bare = props.bare;
   const slot = props.slot || 'main';
   return (
     <div className="Layout">
-      <div className={'Layout__' + slot} role={roles[slot]}>
+      <div
+        className={'Layout__' + slot}
+        role={roles[slot]}
+        style={{ paddingBlock: '1rem', minHeight: 0 }}
+      >
+        {!bare && <DummyBlock thin />}
         {props.children}
+        {!bare && <DummyBlock thin />}
       </div>
     </div>
   );
