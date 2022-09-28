b8b6848640bac97860385d823c668660f7c0552a
Már Örlygsson <mar.nospam@anomy.net>
2022-09-28 12:04:50 +0000

    tests: Add "line" variant to DummyBlock

--- modules/visual-tests/src/layout/DummyBlock.tsx
+++ modules/visual-tests/src/layout/DummyBlock.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import { hannaVars } from '@reykjavik/hanna-css';
+import { EitherObj } from '@reykjavik/hanna-utils';
 
-export type DummyBlockProps = {
-  big?: boolean;
-};
+export type DummyBlockProps = EitherObj<{ big?: true }, { thin?: true }>;
 
 export const DummyBlock = (props: DummyBlockProps) => (
   <div
     className="DummyBlock"
     style={{
-      background: `${hannaVars.color_suld_50} linear-gradient(-45deg,
-        ${hannaVars.color_suld_75} 12.5%,
-        transparent 12.5%,
-        transparent 37.5%,
-        ${hannaVars.color_suld_75} 37.5%,
-        ${hannaVars.color_suld_75} 62.5%,
-        transparent 62.5%,
-        transparent 87.5%,
-        ${hannaVars.color_suld_75} 87.5%
-      )`,
+      background: `${hannaVars.color_suld_50}
+        linear-gradient(-45deg,
+          ${hannaVars.color_suld_75} 12.5%,
+          transparent 12.5%,
+          transparent 37.5%,
+          ${hannaVars.color_suld_75} 37.5%,
+          ${hannaVars.color_suld_75} 62.5%,
+          transparent 62.5%,
+          transparent 87.5%,
+          ${hannaVars.color_suld_75} 87.5%
+        )`,
+      backgroundPosition: '0 0',
       backgroundSize: '32px 32px',
       opacity: 0.5,
-      height: props.big ? '100px' : '50px',
+      height: props.big ? '6.5rem' : props.thin ? '1rem' : '3rem',
     }}
   />
 );
