94a860f1ee95e0924530c2812ae759466be374e8
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2022-10-05 13:23:42 +0000

    test commit

---
+++ modules/hanna-css/src/css/-basics-Kristjan.css.ts
@@ -0,0 +1,7 @@
+import { css } from 'es-in-css';
+
+import { BlockBreak } from './BlockBreak-Kristjan';
+
+export default css`
+  ${BlockBreak};
+`;
---
+++ modules/hanna-css/src/css/BlockBreak-Kristjan.ts
@@ -0,0 +1,10 @@
+import { css } from 'es-in-css';
+
+export const BlockBreak = () => css`
+  .BlockBreak {
+    display: block;
+    width: 100%;
+    height: 0;
+    margin: 0;
+  }
+`;
