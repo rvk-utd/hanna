6e45a24ac0fdd4f6eefd153ff1be88813a1cea75
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2022-10-13 10:51:46 +0000

    Cards added and ImageCards almost completed

---
+++ modules/hanna-css/src/css/ImageCardsKristjan.css.ts
@@ -0,0 +1,139 @@
+import range from '@hugsmidjan/qj/range';
+import { css, ms, pct } from 'es-in-css';
+
+import { between_cols, between_phone_netbook } from '../lib/between';
+import { colors } from '../lib/colors';
+import { hannaVars as vars } from '../lib/hannavars';
+
+import { CardBlock_css, CardList_css, CardListTitle_css } from './styles/cards';
+import {
+  avoidCssnanoMerging,
+  extendBackgroundWithUnderlay,
+  prem,
+} from './utils/miscUtils';
+import {
+  SeenEffect__delay,
+  SeenEffect__fadein,
+  SeenEffect__transition,
+} from './utils/seenEffects';
+
+const borderW = prem(12);
+
+export default css`
+  /*!@deps
+    ButtonTertiary
+  */
+  @media screen {
+    .ImageCards {
+      --ImageCards--fallback: url('/assets/illustrations/esjan.png');
+      ${CardBlock_css}
+      margin-bottom: ${between_cols(60, 110)};
+
+      ${SeenEffect__fadein('.ImageCards__item')};
+      ${SeenEffect__fadein('.ImageCards__summary')};
+
+      ${range(1, 12).map(
+        (i) => css`
+          ${SeenEffect__transition(`.ImageCards__item:nth-child(${i})`)(
+            css`
+              color: red;
+              ${SeenEffect__delay(ms(i * 50 * 200))}
+            `
+          )}
+        `
+      )}
+    }
+
+    .ImageCards--background {
+      ${extendBackgroundWithUnderlay}
+      background-color: ${colors.suld_25};
+
+      padding: 100px 0;
+      ${avoidCssnanoMerging(
+        css`
+          padding: min(${between_phone_netbook(40, 136)}, 136px) 0;
+        `
+      )}
+    }
+
+    .ImageCards__title {
+      ${CardListTitle_css}
+    }
+    .ImageCards__summary {
+      grid-area: title;
+      margin-bottom: ${vars.baseVerticalMargin};
+    }
+    .ImageCards__summary > .ImageCards__title {
+      margin-bottom: ${vars.space_2};
+    }
+
+    .ImageCards__list {
+      ${CardList_css}
+
+      row-gap: 60px;
+
+      ${avoidCssnanoMerging(
+        css`
+          row-gap: min(${between_phone_netbook(48, 80)}, 80px);
+        `
+      )}
+    }
+
+    .ImageCards__item {
+      // …
+    }
+
+    .ImageCards__card {
+      --link-color: _inherit;
+      --link-underline: 1px solid ${colors.suld_200};
+      --link-underline--hover: 1px solid ${colors.faxafloi_100};
+
+      display: block;
+      height: 100%;
+      padding-bottom: ${vars.space_3};
+      ${avoidCssnanoMerging(
+        css`
+          padding-bottom: min(${between_phone_netbook(16, 32)}, ${vars.space_4});
+        `
+      )}
+    }
+
+    .ImageCards__image {
+      display: block;
+      position: relative;
+      padding-top: ${pct(168 / 272)};
+      margin-bottom: ${vars.space_2};
+      ${avoidCssnanoMerging(
+        css`
+          margin-bottom: min(${between_phone_netbook(12, 24)}, ${vars.space_3});
+        `
+      )}
+    }
+    .ImageCards__image--missing {
+      border: ${borderW} solid transparent;
+      padding-top: calc(${pct(168 / 272)} - ${2 * borderW});
+      background: var(--ImageCards--fallback) 50% 50% / contain no-repeat;
+    }
+
+    .ImageCards__image > img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    .ImageCards__card__title {
+      margin-bottom: ${vars.space_1};
+      font: ${vars.font_sh_s};
+      display: block;
+    }
+
+    .ImageCards__card__meta {
+      font: ${vars.font_bd_s};
+      color: ${colors.suld_150};
+      font-weight: ${vars.font_weight__normal};
+    }
+  }
+`;
---
+++ modules/hanna-css/src/css/styles/cards.ts
@@ -0,0 +1,79 @@
+import { css } from 'es-in-css';
+
+import { mq } from '../../lib/breakpoints';
+import { hannaVars as vars } from '../../lib/hannavars';
+import { grid_units, prem } from '../utils/miscUtils';
+
+import { LinkStyle_Reset } from './links';
+
+export const CardBlock_css = () => css`
+  @media ${mq.tablet_up} {
+    display: grid;
+    column-gap: ${vars.grid_gutter};
+    grid-template: 'title list' / ${vars.grid_3} ${vars.grid_9};
+  }
+  @media ${mq.tablet} {
+    grid-template-columns: ${vars.grid_4} ${vars.grid_8};
+  }
+`;
+
+export const CardListTitle_css = () => css`
+  grid-area: title;
+  margin-bottom: ${vars.baseVerticalMargin};
+  font: ${vars.font_hd_s};
+`;
+
+export const CardList_css = () => css`
+  grid-area: list;
+  display: grid;
+  grid-auto-flow: row;
+  gap: ${vars.grid_gutter};
+  grid-template-columns: repeat(auto-fill, ${vars.grid_3});
+
+  @media ${mq.phone} {
+    grid-template-columns: 100%;
+  }
+  @media ${mq.phablet} {
+    grid-template-columns: repeat(auto-fill, ${vars.grid_6});
+  }
+  @media ${mq.tablet} {
+    grid-template-columns: repeat(auto-fill, ${vars.grid_4});
+  }
+`;
+
+export const Card_css = () => css`
+  ${LinkStyle_Reset}
+  display: block;
+  padding: ${prem(24)} ${prem(24)} ${prem(32)} ${prem(24)};
+  min-height: ${prem(168)};
+  height: 100%;
+
+  &::after {
+    content: '';
+    display: block;
+    width: ${grid_units(5)};
+    height: ${prem(2)};
+    background: var(--Card-lineColor, currentColor);
+    margin-top: ${grid_units(2)};
+    transition: width 200ms ease-in;
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
+
+  &__title,
+  &__summary {
+    display: block;
+  }
+
+  &__title {
+    font: ${vars.font_bd_l};
+    font-weight: 700;
+    margin-bottom: ${prem(8)};
+  }
+
+  &__summary {
+    font: ${vars.font_bd_s};
+  }
+`;
