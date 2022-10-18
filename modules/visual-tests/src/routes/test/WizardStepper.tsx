cc9fee05a4a712192b1677d3354dc31e5af34c25
jennyhil <jennyhilduro@gmail.com>
2022-10-18 10:29:19 +0000

    tests: changes to WizardStepper test

--- modules/visual-tests/src/routes/test/WizardStepper.tsx
+++ modules/visual-tests/src/routes/test/WizardStepper.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable no-await-in-loop */
 import React from 'react';
+import { Locator } from '@playwright/test';
 import type { MetaFunction } from '@remix-run/node';
 import WizardStepper, { WizardStepperStep } from '@reykjavik/hanna-react/WizardStepper';
 
 import { DummyBlock } from '../../layout/DummyBlock';
 import { Minimal } from '../../layout/Minimal';
+import { lorem } from '../../test-helpers/dummyData';
 import type { TestingInfo } from '../../test-helpers/testingInfo';
 import { autoTitle } from '../../utils/meta';
 
@@ -11,21 +14,22 @@ export const meta: MetaFunction = autoTitle;
 
 // // Use `handle` if you're using multiple Hanna compnents
 // export const handle = { cssTokens: [], };
-const ACTIVE_STEP = 1;
+const ACTIVE_STEP = 2;
 
 const steps: Array<WizardStepperStep> = [
   { label: 'Inngangur - neutral', clickable: false, done: false, neutral: true },
-  { label: 'Step 1 - done', clickable: false, done: true },
+  { label: 'Not clickable and done', clickable: false, done: true },
   {
-    label: 'Step 2 - clickable with a bit more text than the previous step',
+    label: 'Long clickable text - ' + lorem.medium,
     clickable: true,
     done: false,
   },
-  { label: 'Step 3 - clickable', clickable: true, done: false },
-  { label: 'Step 4', clickable: false, done: false },
+
+  { label: 'Clickable', clickable: true, done: false },
+  { label: 'Not clickable', clickable: false, done: true },
   { label: '', clickable: false, done: false },
   {
-    label: 'Staðfesting (always clickable)',
+    label: 'Always clickable',
     clickable: 'always',
   },
 ];
@@ -35,18 +39,12 @@ export default function () {
     // Minimal is a no-frills, no-chrome replacement for the `Layout` component,
     <Minimal>
       <br />
-      <WizardStepper
-        steps={steps}
-        activeStep={Math.floor(steps.length / 2)}
-        onClick={() => {
-          ('');
-        }}
-      />
+      <WizardStepper steps={steps} activeStep={ACTIVE_STEP} onClick={() => ''} />
       <DummyBlock thin />
       <br />
       <WizardStepper
         steps={steps.slice(0, ACTIVE_STEP + 1)}
-        activeStep={ACTIVE_STEP}
+        activeStep={ACTIVE_STEP - 1}
         onClick={() => {
           ('');
         }}
@@ -56,21 +54,39 @@ export default function () {
 }
 
 export const testing: TestingInfo = {
-  extras: async ({ page, pageScreenshot, localScreenshot }) => {
-    const stepTwo = page.locator('.WizardStepper__step:text("Step 2")');
-    const alwaysClickable = page.locator('.WizardStepper__step:text("always clickable")');
-    const notClickable = page.locator('.WizardStepper__step:text("Step 4")');
+  extras: async ({ page, localScreenshot }) => {
+    const neutral = page.locator('.WizardStepper__step--neutral >> nth = 0');
+    const activeClickable = page.locator(
+      '.WizardStepper__step:text("Long clickable") >> nth = 0'
+    );
+    const clickable = page.locator('.WizardStepper__step:text-is("Clickable")');
+    const alwaysClickable = page.locator(
+      '.WizardStepper__step:text-is("Always clickable")'
+    );
+    // Hover/focus/click not clickable ?
+    // const notClickable = page.locator('');
 
-    await stepTwo.focus();
-    await pageScreenshot('wizardStepper-focus');
+    const testItems = [
+      { loc: neutral, name: 'neutral' },
+      { loc: activeClickable, name: 'activeClickable' },
+      { loc: alwaysClickable, name: 'alwaysClickable' },
+      { loc: clickable, name: 'clickable' },
+    ];
 
-    await alwaysClickable.click();
-    await pageScreenshot('alwaysClickable-click');
+    const test = async (item: Locator, name: string) => {
+      await item.hover({ force: true });
+      await localScreenshot(item, name + '-hover', { margin: true });
 
-    await stepTwo.hover();
-    await pageScreenshot('wizardStepper-hover');
+      await page.mouse.move(0, 0);
 
-    await notClickable.hover({ force: true });
-    await localScreenshot(notClickable, 'notClickable-hover', { margin: true });
+      await item.focus();
+      await localScreenshot(item, name + '-focus', { margin: true });
+    };
+
+    let i = 0;
+    let item: typeof testItems[number] | undefined;
+    while ((item = testItems[i++])) {
+      await test(item.loc, item.name);
+    }
   },
 };
