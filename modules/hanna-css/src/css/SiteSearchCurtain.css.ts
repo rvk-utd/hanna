746996c8145c3060c2e80732ce68d62bd8f19bf9
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2022-10-25 12:56:59 +0000

    SiteSearchCurtain converted to ts

---
+++ modules/hanna-css/src/css/SiteSearchCurtain.css.ts
@@ -0,0 +1,50 @@
+import { css } from 'es-in-css';
+
+import { mq } from '../lib/breakpoints';
+import { colors } from '../lib/colors';
+import { hannaVars as vars } from '../lib/hannavars';
+
+export default css`
+  /*!@deps
+    SiteSearchAutocomplete
+  */
+
+  @media screen {
+    .SiteSearchCurtain {
+      position: relative;
+      z-index: ${vars.zindex__overlay};
+    }
+
+    @media ${mq.tablet_up} {
+      .SiteSearchCurtain > .SiteSearchAutocomplete {
+        width: 100%;
+        transition: width 200ms ease-in;
+      }
+      .SiteSearchCurtain--focused > .SiteSearchAutocomplete {
+        width: ${vars.grid_7_7};
+      }
+    }
+
+    .SiteSearchCurtain::after {
+      content: '';
+      position: fixed;
+      z-index: -1;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      pointer-events: none;
+      background-color: rgba(${colors.suld_0}, 0.9);
+
+      opacity: 0;
+      visibility: hidden;
+      transition: opacity 200ms ease-in, visibility 0ms 200ms;
+    }
+    .SiteSearchCurtain--focused::after {
+      pointer-events: auto;
+      transition-delay: 0ms;
+      visibility: visible;
+      opacity: 1;
+    }
+  }
+`;
