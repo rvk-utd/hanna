af4be49222aa2bdadfd3864fbb47c79cd8b30ee4
Már Örlygsson <mar.nospam@anomy.net>
2022-10-31 14:33:57 +0000

    tests: Add token tests for the `shareButtonUtils` entry point

---
+++ modules/hanna-utils/src/shareButtonsUtils.tests.ts
@@ -0,0 +1,30 @@
+import { reportKeyMismatch } from 'hanna-test-helpers/ospec';
+import o from 'ospec';
+
+import * as lib from './shareButtonsUtils';
+
+type ExpectedExports = keyof typeof lib;
+
+o.spec('hanna-utils/shareButtonsUtils', () => {
+  o('exports the correct tokens', () => {
+    const expectedTokens: Record<ExpectedExports, true> = {
+      getDocMeta: true,
+      getShareButtonLabel: true,
+      openInPopup: true,
+      shareButtonTexts: true,
+      shareButtonTypes: true,
+    };
+
+    reportKeyMismatch(lib, expectedTokens);
+  });
+});
+
+/* eslint-disable @typescript-eslint/no-unused-vars, unused-imports/no-unused-imports-ts, import/first, simple-import-sort/imports */
+// Also check exported types. (Ignoring re-exported types from es-in-css.)
+import type {
+  // assets.ts
+  DocMeta,
+  ShareButtonI18n,
+  ShareButtonPlatforms,
+} from './shareButtonsUtils';
+/* eslint-enable @typescript-eslint/no-unused-vars, unused-imports/no-unused-imports-ts, import/first, simple-import-sort/imports */
