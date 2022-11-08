99933d348bf69fb98b12cbaa706c0b41d20005a5
Már Örlygsson <mar.nospam@anomy.net>
2022-11-08 11:25:58 +0000

    fix: Make `ObjectFromEntries` accept readonly arrays

--- modules/hanna-utils/src/_/ObjectHelpers.ts
+++ modules/hanna-utils/src/_/ObjectHelpers.ts
@@ -3,8 +3,9 @@ export const ObjectEntries = <T extends object>(object: T) =>
   Object.entries(object) as Array<[keyof T, T[keyof T]]>;
 
 /** Hack to add some typing to Object.fromEntries */
-export const ObjectFromEntries = <K extends string, V>(array: Array<[K, V]>) =>
-  Object.fromEntries(array) as Record<K, V>;
+export const ObjectFromEntries = <K extends string, V>(
+  array: ReadonlyArray<readonly [K, V]>
+) => Object.fromEntries(array) as Record<K, V>;
 
 /** Hack to add some typing to Object.keys */
 export const ObjectKeys = <T extends object>(object: T) =>
