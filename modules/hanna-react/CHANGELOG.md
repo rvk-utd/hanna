af17831ecffc0260b9293bc5015de5696de3ac3e
Már Örlygsson <mar.nospam@anomy.net>
2022-12-15 11:17:41 +0000

    feat: Add `setLinkRenderer.pop()` to unset the last custom renderer

--- modules/hanna-react/CHANGELOG.md
+++ modules/hanna-react/CHANGELOG.md
@@ -3,6 +3,7 @@
 ## Upcoming...
 
 - ... <!-- Add new lines here. -->
+- feat: Add `setLinkRenderer.pop()` to unset the last custom renderer
 - fix: Suppress react warning about mixed-case `data-*` attributes
 
 ## 0.10.75
--- modules/hanna-react/src/_abstract/_Link.tsx
+++ modules/hanna-react/src/_abstract/_Link.tsx
@@ -10,6 +10,8 @@ const DefaultLinkRenderer: LinkRenderer = (props) => React.createElement('a', pr
 
 export let Link = DefaultLinkRenderer;
 
+const history: Array<LinkRenderer> = [];
+
 /**
  * Allows you to set a callback function to wrapp <a href=""/> links with
  * a custom routing component.
@@ -17,17 +19,46 @@ export let Link = DefaultLinkRenderer;
  * Example use:
  *
  * ```js
- *   import { Link } from 'remix'; // or whatever :-)
+ * import { Link } from 'remix'; // or whatever :-)
  *
- *   setLinkRenderer((linkProps) =>
- *     <Link to={linkProps.href}><a {...linkProps} /></Link>);
+ * setLinkRenderer((linkProps) =>
+ *   <Link to={linkProps.href}><a {...linkProps} /></Link>);
+ * ```
+ *
+ * The link renderers are pushed to a simple stack, and if you want to unset
+ * a custom renderer, use the `setLinkRenderer.pop()` method to go back to
+ * the previous one. Example:
+ *
+ * ```js
+ * const BlockLinks = (props) =>
+ *   <span className={props.className}>[Link temporarily out of order]</span>
+ *
+ * setLinkRenderer(BlockLinks);
+ * // ...render some Hanna components...
+ * setLinkRenderer.pop(); // reset link rendering
  * ```
  *
  * The Default linkrenderer defined as:
  * ```js
  * 	(linkProps) => React.createElement('a', linkProps);
  * ```
+ *
+ * ...and you can explicitly switch to using this default by passing `undefined`
+ * as an argument — like so:
+ *
+ * ```js
+ * setLinkRenderer(undefined);
+ * ```
  */
 export const setLinkRenderer = (linkRenderer: LinkRenderer | undefined) => {
   Link = linkRenderer || DefaultLinkRenderer;
+  history.unshift(Link);
+};
+
+/**
+ *
+ */
+setLinkRenderer.pop = () => {
+  history.shift();
+  Link = history[0] || DefaultLinkRenderer;
 };
