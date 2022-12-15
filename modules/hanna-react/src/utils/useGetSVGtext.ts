28f407a9e8138509d8e5e4157c3af004559a9c37
Már Örlygsson <mar.nospam@anomy.net>
2022-12-15 09:34:55 +0000

    ldocs: Add JSDoc for `useGetSVGtext()`

--- modules/hanna-react/src/utils/useGetSVGtext.ts
+++ modules/hanna-react/src/utils/useGetSVGtext.ts
@@ -3,6 +3,10 @@ import { getSVGtext } from '@reykjavik/hanna-utils';
 
 type SVGMeta = { imageSrc: string; code: string };
 
+/**
+ * Fetches a remote SVG image and returns its XML/source
+ * string for inlining, or further processing
+ */
 export const useGetSVGtext = (imageSrc: string | undefined): SVGMeta | undefined => {
   const [inlineSvg, setInlineSvg] = useState<SVGMeta>();
   const srcRef = useRef(imageSrc);
