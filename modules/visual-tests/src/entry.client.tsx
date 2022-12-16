a86e72350dd87937f17c88cc53e3e45712e0bb76
Már Örlygsson <mar.nospam@anomy.net>
2022-12-16 11:25:17 +0000

    chore: Update Remix to latest version

--- modules/visual-tests/src/entry.client.tsx
+++ modules/visual-tests/src/entry.client.tsx
@@ -2,4 +2,12 @@ import React from 'react';
 import { hydrate } from 'react-dom';
 import { RemixBrowser } from '@remix-run/react';
 
-hydrate(<RemixBrowser />, document);
+const start = () => hydrate(<RemixBrowser />, document);
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(start);
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  window.setTimeout(start, 1);
+}
