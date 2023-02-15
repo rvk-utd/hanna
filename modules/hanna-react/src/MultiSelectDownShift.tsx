31cea698d5f5c0b0ae662336c89db1938f97c568
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-02-15 10:45:10 +0000

    updated example

--- modules/hanna-react/src/MultiSelectDownShift.tsx
+++ modules/hanna-react/src/MultiSelectDownShift.tsx
@@ -1,49 +1,58 @@
-import React from 'react';
-import { useMultipleSelection, useSelect, UseSelectStateChangeTypes } from 'downshift';
+import React, { useMemo, useState } from 'react';
+import { useCombobox, useMultipleSelection } from 'downshift';
 
-type ItemGaur = {
-  value: string;
-  label: string;
+type Book = {
+  author: string;
+  title: string;
 };
 
-const itemsList = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' },
-  { value: 'coffee', label: 'Coffee' },
-  { value: 'mint-chocolate-chip', label: 'Mint Chocolate Chip' },
-  { value: 'rocky-road', label: 'Rocky Road' },
-  { value: 'cookies-and-cream', label: 'Cookies and Cream' },
-  { value: 'butter-pecan', label: 'Butter Pecan' },
-  { value: 'pistachio', label: 'Pistachio' },
-  { value: 'maple-walnut', label: 'Maple Walnut' },
-  { value: 'caramel', label: 'Caramel' },
-  { value: 'fudge', label: 'Fudge' },
+const books = [
+  { author: 'Harper Lee', title: 'To Kill a Mockingbird' },
+  { author: 'Lev Tolstoy', title: 'War and Peace' },
+  { author: 'Fyodor Dostoyevsy', title: 'The Idiot' },
+  { author: 'Oscar Wilde', title: 'A Picture of Dorian Gray' },
+  { author: 'George Orwell', title: '1984' },
+  { author: 'Jane Austen', title: 'Pride and Prejudice' },
+  { author: 'Marcus Aurelius', title: 'Meditations' },
+  { author: 'Fyodor Dostoevsky', title: 'The Brothers Karamazov' },
+  { author: 'Lev Tolstoy', title: 'Anna Karenina' },
+  { author: 'Fyodor Dostoevsky', title: 'Crime and Punishment' },
 ];
 
-const MultiSelectDownshift = () => {
-  const {
-    getSelectedItemProps,
-    selectedItems,
-    removeSelectedItem,
-    addSelectedItem,
-    getDropdownProps,
-  } = useMultipleSelection<ItemGaur | undefined>({
-    initialSelectedItems: [itemsList[0], itemsList[1]],
-  });
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const initialSelectedItems: Array<Book> = [books[0]!, books[1]!];
 
-  const { getLabelProps, getMenuProps, getItemProps, selectItem, getToggleButtonProps } =
-    useSelect({
-      items: itemsList,
-      onStateChange: ({ type, selectedItem }) => {
+const getFilteredBooks = (selectedItems: Array<Book>, inputValue: string) => {
+  const lowerCasedInputValue = inputValue.toLowerCase();
+
+  return books.filter(function filterBook(book) {
+    return (
+      !selectedItems.includes(book) &&
+      (book.title.toLowerCase().includes(lowerCasedInputValue) ||
+        book.author.toLowerCase().includes(lowerCasedInputValue))
+    );
+  });
+};
+
+const MultiSelectDownshift = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [selectedItems, setSelectedItems] = useState<Array<Book>>(initialSelectedItems);
+  const items = useMemo(
+    () => getFilteredBooks(selectedItems, inputValue),
+    [selectedItems, inputValue]
+  );
+
+  const { getSelectedItemProps, getDropdownProps, addSelectedItem, removeSelectedItem } =
+    useMultipleSelection({
+      selectedItems,
+      onStateChange({ selectedItems: newSelectedItems, type }) {
         switch (type) {
-          case UseSelectStateChangeTypes.ToggleButtonKeyDownEnter:
-          case UseSelectStateChangeTypes.ToggleButtonKeyDownSpaceButton:
-          case UseSelectStateChangeTypes.ItemClick:
-          case UseSelectStateChangeTypes.ToggleButtonBlur:
-            if (selectedItem) {
-              addSelectedItem(selectedItem);
-              selectItem(null);
+          case useMultipleSelection.stateChangeTypes.SelectedItemKeyDownBackspace:
+          case useMultipleSelection.stateChangeTypes.SelectedItemKeyDownDelete:
+          case useMultipleSelection.stateChangeTypes.DropdownKeyDownBackspace:
+          case useMultipleSelection.stateChangeTypes.FunctionRemoveSelectedItem:
+            if (newSelectedItems) {
+              setSelectedItems(newSelectedItems);
             }
             break;
           default:
@@ -52,28 +61,121 @@ const MultiSelectDownshift = () => {
       },
     });
 
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    getInputProps,
+    highlightedIndex,
+    getItemProps,
+    selectedItem,
+  } = useCombobox({
+    items,
+    itemToString(item) {
+      return item ? item.title : '';
+    },
+    defaultHighlightedIndex: 0, // after selection, highlight the first item.
+    selectedItem: null,
+    stateReducer(state, actionAndChanges) {
+      const { changes, type } = actionAndChanges;
+
+      switch (type) {
+        case useCombobox.stateChangeTypes.InputKeyDownEnter:
+        case useCombobox.stateChangeTypes.ItemClick:
+        case useCombobox.stateChangeTypes.InputBlur:
+          return {
+            ...changes,
+            ...(changes.selectedItem && { isOpen: true, highlightedIndex: 0 }),
+          };
+        default:
+          return changes;
+      }
+    },
+    onStateChange({ inputValue: newInputValue, type, selectedItem: newSelectedItem }) {
+      switch (type) {
+        case useCombobox.stateChangeTypes.InputKeyDownEnter:
+        case useCombobox.stateChangeTypes.ItemClick: {
+          const newArray = [...selectedItems, newSelectedItem];
+          setSelectedItems(newArray);
+          break;
+        }
+
+        case useCombobox.stateChangeTypes.InputChange:
+          if (newInputValue) {
+            setInputValue(newInputValue);
+          }
+          break;
+        default:
+          break;
+      }
+    },
+  });
+
   return (
     <div>
-      <p>MultiSelect</p>
-      <label {...getLabelProps()}>Choose a flavour</label>
-      {selectedItems.map((selectedItem, index) => (
-        <span
-          key={`selected-item-${index}`}
-          {...getSelectedItemProps({ selectedItem, index })}
+      <p>MultiSelect with Downshift</p>
+      <div className="w-[592px]">
+        <div className="flex flex-col gap-1">
+          <label className="w-fit" {...getLabelProps()}>
+            Pick some books:
+          </label>
+          <div className="shadow-sm bg-white inline-flex gap-2 items-center flex-wrap p-1.5">
+            {selectedItems.map(function renderSelectedItem(selectedItemForRender, index) {
+              return (
+                <span
+                  className="bg-gray-100 rounded-md px-1 focus:bg-red-400"
+                  key={`selected-item-${index}`}
+                  {...getSelectedItemProps({
+                    selectedItem: selectedItemForRender,
+                    index,
+                  })}
+                >
+                  {selectedItemForRender.title}
+                  <span
+                    className="px-1 cursor-pointer"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      removeSelectedItem(selectedItemForRender);
+                    }}
+                  >
+                    &#10005;
+                  </span>
+                </span>
+              );
+            })}
+            <div className="flex gap-0.5 grow">
+              <input
+                placeholder="Best book ever"
+                className="w-full"
+                {...getInputProps(getDropdownProps({ preventKeyAction: isOpen }))}
+              />
+              <button
+                aria-label="toggle menu"
+                className="px-2"
+                type="button"
+                {...getToggleButtonProps()}
+              >
+                &#8595;
+              </button>
+            </div>
+          </div>
+        </div>
+        <ul
+          className={`absolute w-inherit bg-white mt-1 shadow-md max-h-80 overflow-scroll p-0 ${
+            !(isOpen && items.length) && 'hidden'
+          }`}
+          {...getMenuProps()}
         >
-          {selectedItem?.label}
-          <span onClick={() => removeSelectedItem(selectedItem)}>&#10005;</span>
-        </span>
-      ))}
-
-      <button {...getToggleButtonProps(getDropdownProps())}>Elements</button>
-      <ul {...getMenuProps()}>
-        {itemsList.map((item, index) => (
-          <li key={`${item}${index}`} {...getItemProps({ item, index })}>
-            {item.label}
-          </li>
-        ))}
-      </ul>
+          {isOpen &&
+            items.map((item, index) => (
+              <li key={`${item.title}${index}`} {...getItemProps({ item, index })}>
+                <span>{item.title}</span>
+                <span className="text-sm text-gray-700">{item.author}</span>
+              </li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 };
