a06f0f05b819cb661695b48a9640709cae27324a
Már Örlygsson <mar.nospam@anomy.net>
2023-03-22 16:10:10 +0000

    style: Remove workaround obviated by commit 168b7784

--- modules/hanna-css/src/css/-basics.css.ts
+++ modules/hanna-css/src/css/-basics.css.ts
@@ -165,16 +165,6 @@ export default css`
     quotes: ${quotes('PL')};
   }
 
-  // nextjs@11 injects this and it triggers scrolling bugs
-  // (including double-scrollbar) in Chrome/Blink.
-  // (Relevant PR: https://github.com/vercel/next.js/pull/44853)
-  // Next.js is common-enough consumer of the Hanna CSS
-  // to justify special handling here.
-  next-route-announcer {
-    position: absolute;
-    top: 0;
-  }
-
   *:first-child {
     margin-top: 0;
   }
