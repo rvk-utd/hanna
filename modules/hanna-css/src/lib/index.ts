73dc5b4c694879f469ec77df9e2a0de6b2abdfd1
Már Örlygsson <mar.nospam@anomy.net>
2023-04-25 11:04:15 +0000

    style: Auto-sort imports

--- modules/hanna-css/src/lib/index.ts
+++ modules/hanna-css/src/lib/index.ts
@@ -1,12 +1,3 @@
-export * from './cssutils.js';
-export type { HannaCssVarToken } from './hannavars.js';
-export { hannaVarOverride, hannaVars } from './hannavars.js';
-export type { IconName } from './icons.js';
-export { characters, iconfont_raw, icons, iconStyle } from './icons.js';
-export type { HannaColorTheme } from './themes.js';
-export { colorThemes } from './themes.js';
-export { WARNING__, WARNING_message__, WARNING_soft__ } from './WARNING__.js';
-// Re-export all of es-in-css for convenience
 export {
   srOnly,
   srOnly__undo,
@@ -17,6 +8,14 @@ export { bp as breakpoints_raw, mq } from './breakpoints.js';
 export { htmlCl } from './classNames.js';
 export type { ColorFamily } from './colors.js';
 export { colorFamilies, colors as colors_raw } from './colors.js';
+export * from './cssutils.js';
 export { font as font_raw } from './font.js';
 export { grid as grid_raw } from './grid.js';
-export * from 'es-in-css';
+export type { HannaCssVarToken } from './hannavars.js';
+export { hannaVarOverride, hannaVars } from './hannavars.js';
+export type { IconName } from './icons.js';
+export { characters, iconfont_raw, icons, iconStyle } from './icons.js';
+export type { HannaColorTheme } from './themes.js';
+export { colorThemes } from './themes.js';
+export { WARNING__, WARNING_message__, WARNING_soft__ } from './WARNING__.js';
+export * from 'es-in-css'; // Re-export all of es-in-css for convenience
