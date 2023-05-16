2f28c5fcfadb7e8f7444dcd09c2dc2fb582ce836
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-16 12:08:57 +0000

    WizardStepper and addon controls package add

---
+++ modules/html-storybook-new/src/stories/WizardStepper.stories.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import Tooltip from '@reykjavik/hanna-react/Tooltip';
+import { WizardStepper, WizardStepperStep } from '@reykjavik/hanna-react/WizardStepper';
+import { optionsKnob } from '@storybook/addon-knobs';
+import { Meta, StoryObj } from '@storybook/react';
+
+const steps: Array<WizardStepperStep> = [
+  {
+    label: 'Inngangur',
+    clickable: true,
+    neutral: true,
+  },
+  {
+    label: 'Ástæða viðtals (never clickable)',
+  },
+  {
+    label: 'Bæta við gögnum',
+    clickable: true,
+  },
+  {
+    label: 'Við höfum samband',
+    clickable: true,
+  },
+  {
+    label: 'Aðeins meira (custom "done" status)',
+    clickable: true,
+    done: true,
+  },
+  {
+    label: 'Staðfesting (always clickable)',
+    clickable: 'always',
+  },
+];
+
+const demoSteps: Record<string, string> = {
+  None: 'none',
+};
+for (let i = 0; i < steps.length; i++) {
+  demoSteps[i ? 'Step ' + i : 'Intro'] = String(i);
+}
+
+const meta: Meta<typeof Tooltip> = {
+  title: 'components/WizardStepper',
+  component: Tooltip,
+};
+export default meta;
+
+type Story = StoryObj<typeof Tooltip>;
+
+const WizardStepperComponent = () => {
+  let activeStepFromKnob: number | undefined = Number(
+    optionsKnob('Active step', demoSteps, '1', { display: 'inline-radio' })
+  );
+
+  if (isNaN(activeStepFromKnob)) {
+    activeStepFromKnob = undefined;
+  }
+
+  const [activeStep, setActiveStep] = useState(activeStepFromKnob);
+
+  useEffect(() => {
+    setActiveStep(activeStepFromKnob);
+  }, [activeStepFromKnob]);
+
+  return (
+    <WizardStepper
+      steps={steps}
+      activeStep={activeStep}
+      onClick={setActiveStep}
+      allowForwardSkip={false}
+      disableBacktrack={false}
+    />
+  );
+};
+
+export const _WizardStepper: Story = {
+  render: () => <WizardStepperComponent />,
+};
