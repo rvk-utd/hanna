93f6a0985eab8c800231d2fe1ea98e11891f6707
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-17 08:50:26 +0000

    css link init

--- modules/html-storybook-new/.storybook/preview.tsx
+++ modules/html-storybook-new/.storybook/preview.tsx
@@ -1,6 +1,20 @@
+import React from 'react';
 import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
+  decorators: [
+    (Story) => {
+      return (
+        <>
+          <link
+            rel="stylesheet"
+            href="https://styles.reykjavik.is/bundle/v0.8?m=-basics,Layout,Tooltip"
+          />
+          {Story()}
+        </>
+      );
+    },
+  ],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
--- modules/html-storybook-new/src/stories/Tooltip.stories.ts
+++ modules/html-storybook-new/src/stories/Tooltip.stories.ts
@@ -1,12 +1,13 @@
 import Tooltip from '@reykjavik/hanna-react/Tooltip';
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { loremRT } from '../../../visual-tests/src/test-helpers/dummyData';
+import { loremRT } from '../../../visual-tests/src/test-helpers/dummyData.js';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof Tooltip> = {
   title: 'Components/Tooltip',
   component: Tooltip,
+  // decorators: [withCSS('https://styles.reykjavik.is/css/v0.8/Tooltip.css')],
   args: {
     label: 'Hover me',
     text: loremRT.short(true),
