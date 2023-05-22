c3687fe853f542354783cc75b45b6f10b11207fc
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-22 13:12:49 +0000

    wizardLayout add

---
+++ modules/html-storybook-new/src/stories/WizardLayout.stories.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Alert } from '@reykjavik/hanna-react/Alert';
+import { WizardLayout } from '@reykjavik/hanna-react/WizardLayout';
+import { WizardLayoutClose } from '@reykjavik/hanna-react/WizardLayoutClose';
+import { WizardStepper } from '@reykjavik/hanna-react/WizardStepper';
+import { boolean } from '@storybook/addon-knobs';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof WizardLayout> = {
+  title: 'components/Layout/WizardLayout',
+  component: WizardLayout,
+};
+export default meta;
+
+type Story = StoryObj<typeof WizardLayout>;
+
+const MinimalWizardLayoutComponent = () => {
+  const globalAlerts = boolean('Global alerts container', false);
+  return <WizardLayout key={'' + globalAlerts} globalAlerts={globalAlerts && ' '} />;
+};
+
+export const _MinimalWizardLayout: Story = {
+  render: () => <MinimalWizardLayoutComponent />,
+};
+
+const steps = [
+  {
+    label: 'Fyrir hvern er viðtalið?',
+    clickable: true,
+  },
+  {
+    label: 'Ástæða viðtals (not clickable)',
+  },
+  {
+    label: 'Bæta við gögnum',
+    clickable: true,
+  },
+  {
+    label: 'Við höfum samband',
+    clickable: true,
+  },
+];
+
+const WizardLayoutWithContentComponent = () => {
+  const globalAlerts = boolean('Global alerts container', false);
+  return (
+    <WizardLayout
+      key={'' + globalAlerts}
+      globalAlerts={
+        globalAlerts && (
+          <>
+            <Alert type="warning" closable>
+              Some warning
+            </Alert>
+            <Alert type="critical" closable>
+              Some critical alert
+            </Alert>
+          </>
+        )
+      }
+      wizardStepper={
+        <WizardStepper steps={steps} activeStep={0} onClick={() => undefined} />
+      }
+      wizardFooter={
+        <WizardLayoutClose onClick={() => undefined}>Hætta</WizardLayoutClose>
+      }
+    >
+      <p style={{ height: '200vh' }}>Form</p>
+    </WizardLayout>
+  );
+};
+
+export const _WizardLayoutWithContent: Story = {
+  render: () => <WizardLayoutWithContentComponent />,
+};
