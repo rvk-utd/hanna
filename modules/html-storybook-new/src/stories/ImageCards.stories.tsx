69787a05077f74c18bfb1bd28bdf403cf2d60f6b
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-22 15:15:52 +0000

    ImageCards add

---
+++ modules/html-storybook-new/src/stories/ImageCards.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ButtonTertiary } from '@reykjavik/hanna-react/ButtonTertiary';
+import { ImageCards } from '@reykjavik/hanna-react/ImageCards';
+import { getIllustrationUrl } from '@reykjavik/hanna-utils/assets';
+import { boolean } from '@storybook/addon-knobs';
+import { Meta, StoryObj } from '@storybook/react';
+
+import { imageCards } from '../utils/_dummyData.js';
+
+const meta: Meta<typeof ImageCards> = {
+  title: 'components/ImageCards',
+  component: ImageCards,
+};
+export default meta;
+
+type Story = StoryObj<typeof ImageCards>;
+
+const Component = () => {
+  const background = boolean('Background color', false) || undefined;
+  const imgPlaceholder = boolean('Custom fallback image', false)
+    ? getIllustrationUrl('framkvaemdir2')
+    : undefined;
+  const title = boolean('Title', true) || undefined;
+  const summary = boolean('Summary text/More link', false) || undefined;
+  return (
+    <ImageCards
+      title={title && 'Image Cards'}
+      summaryElement={summary && <ButtonTertiary href="">Sjá yfirlit</ButtonTertiary>}
+      background={background}
+      cards={imageCards}
+      imgPlaceholder={imgPlaceholder}
+      startSeen
+    />
+  );
+};
+
+export const _ImageCards: Story = {
+  render: () => <Component />,
+};
