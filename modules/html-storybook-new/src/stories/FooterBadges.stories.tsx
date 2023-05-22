2323198dbda60e4c6d28bdd09819bd3143b987bf
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-22 10:57:23 +0000

    FooterBadges add

---
+++ modules/html-storybook-new/src/stories/FooterBadges.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FooterBadges, FooterBadgesProps } from '@reykjavik/hanna-react/FooterBadges';
+import { Meta, StoryObj } from '@storybook/react';
+
+import dummyImage from '../example_assets/FooterBadges__image.jpg';
+
+const meta: Meta<typeof FooterBadges> = {
+  title: 'components/Layout/FooterBadges',
+  component: FooterBadges,
+};
+export default meta;
+
+type Story = StoryObj<typeof FooterBadges>;
+
+const dummySubFooterLinks: FooterBadgesProps['badges'] = [
+  {
+    href: 'http://www.reykjavik.is',
+    altText: 'Jafnlaunavottun 2020–2021',
+    src: dummyImage,
+  },
+];
+
+dummySubFooterLinks.push(...dummySubFooterLinks);
+
+export const _FooterBadges: Story = {
+  render: () => <FooterBadges badges={dummySubFooterLinks} />,
+};
