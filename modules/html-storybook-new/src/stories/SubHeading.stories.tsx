ff8cc03bf7db0a44c8b8b9e35758f2a4311e57cc
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-22 10:40:50 +0000

    SubHeading add

---
+++ modules/html-storybook-new/src/stories/SubHeading.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { SubHeading } from '@reykjavik/hanna-react/SubHeading';
+import { boolean, optionsKnob } from '@storybook/addon-knobs';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof SubHeading> = {
+  title: 'components/text/SubHeading',
+  component: SubHeading,
+};
+export default meta;
+
+type Story = StoryObj<typeof SubHeading>;
+
+const Component = () => {
+  const layout =
+    optionsKnob(
+      'Layout',
+      {
+        Left: '',
+        'Right aligned': 'right',
+        Wide: 'wide',
+      },
+      '',
+      { display: 'inline-radio' }
+    ) || undefined;
+
+  const layoutProps = layout === 'wide' ? { wide: true } : { align: layout };
+
+  const small = boolean('Small', false) || undefined;
+
+  const htmlTag =
+    optionsKnob('Heading level', { 'H2 (default)': '', H3: 'h3' }, '', {
+      display: 'inline-radio',
+    }) || undefined;
+  return (
+    <SubHeading {...layoutProps} small={small} Tag={htmlTag} startSeen>
+      Subheading title
+    </SubHeading>
+  );
+};
+
+export const _SubHeading: Story = {
+  render: () => <Component />,
+};
