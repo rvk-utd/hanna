7f0c3e04b2bf3f51cf4c16c7df2018ee4236f8f4
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-22 11:16:48 +0000

    MinimalLayout add

---
+++ modules/html-storybook-new/src/stories/MinimalLayout.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Layout } from '@reykjavik/hanna-react/Layout';
+import { boolean } from '@storybook/addon-knobs';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Layout> = {
+  title: 'components/Layout/MinimalLayout',
+  component: Layout,
+};
+export default meta;
+
+type Story = StoryObj<typeof Layout>;
+
+const Component = () => {
+  const globalAlerts = boolean('Global alerts container', false);
+  const navChildren = boolean('Page has no menu or navigation', false) ? undefined : ' ';
+  return (
+    <Layout
+      key={'' + globalAlerts + navChildren}
+      globalAlerts={globalAlerts && ' '}
+      navChildren={navChildren}
+      ssr="ssr-only"
+      mainChildren=""
+    />
+  );
+};
+
+export const _MinimalLayout: Story = {
+  render: () => <Component />,
+};
