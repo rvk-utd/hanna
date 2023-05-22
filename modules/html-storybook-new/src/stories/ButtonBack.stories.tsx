4a211802b0567cf1ae98d4a63614b69a57e2c838
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-22 13:18:43 +0000

    buttonBack add

---
+++ modules/html-storybook-new/src/stories/ButtonBack.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ButtonBack } from '@reykjavik/hanna-react/ButtonBack';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof ButtonBack> = {
+  title: 'components/buttons/ButtonBack',
+  component: ButtonBack,
+};
+export default meta;
+
+type Story = StoryObj<typeof ButtonBack>;
+
+const Component = () => {
+  return (
+    <>
+      <p>
+        <ButtonBack>Button Back</ButtonBack>
+        <ButtonBack disabled>Disabled</ButtonBack>
+      </p>
+      <br />
+      <p>
+        <ButtonBack href="">Link Back</ButtonBack>{' '}
+      </p>
+    </>
+  );
+};
+
+export const _ButtonBack: Story = {
+  render: () => <Component />,
+};
