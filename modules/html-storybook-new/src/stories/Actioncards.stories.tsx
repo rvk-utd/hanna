55f5cc35a2a3c985d95ee6599b62ab9bde689d93
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-22 09:03:17 +0000

    ActionCards add

---
+++ modules/html-storybook-new/src/stories/Actioncards.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import range from '@hugsmidjan/qj/range';
+import { ActionCards, ActionCardsItemProps } from '@reykjavik/hanna-react/ActionCards';
+import { ButtonTertiary } from '@reykjavik/hanna-react/ButtonTertiary';
+import { boolean } from '@storybook/addon-knobs';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof ActionCards> = {
+  title: 'components/ActionCards',
+  component: ActionCards,
+};
+export default meta;
+
+type Story = StoryObj<typeof ActionCards>;
+
+const cards = range(1, 8).map(
+  (n): ActionCardsItemProps => ({
+    title: 'ActionCard ' + n,
+    href: '',
+    summary:
+      n % 4 === 0
+        ? undefined
+        : (n + 3) % 3 === 0
+        ? 'Summary for this card lorem ipsum dolor sit bladiblah foo blah'
+        : 'Summary for this card',
+  })
+);
+
+const Component = () => {
+  const title = boolean('Title', true) || undefined;
+  const summary = boolean('Summary text/More link', false) || undefined;
+  return (
+    <ActionCards
+      title={title && 'Action Cards'}
+      summaryElement={summary && <ButtonTertiary href="">Sjá yfirlit</ButtonTertiary>}
+      cards={cards}
+      startSeen
+    />
+  );
+};
+
+export const _ActionCards: Story = {
+  render: () => <Component />,
+};
