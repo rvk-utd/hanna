5ffe78ff82c5156468711eb87686e08e84b31b1c
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-22 15:55:06 +0000

    Minimetrics add

---
+++ modules/html-storybook-new/src/stories/MiniMetrics.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { MiniMetrics } from '@reykjavik/hanna-react/MiniMetrics';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof MiniMetrics> = {
+  title: 'components/MiniMetrics',
+  component: MiniMetrics,
+};
+export default meta;
+
+type Story = StoryObj<typeof MiniMetrics>;
+
+export const _MiniMetrics: Story = {
+  render: () => (
+    <MiniMetrics
+      text="78% nemanda upplifðu gleði 2019"
+      moreButton={{ href: '', label: 'Skoða mælaborðið' }}
+      startSeen
+    />
+  ),
+};
