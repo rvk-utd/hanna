c343f6e0983414f24319e4481c017e829bdfaa5c
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-22 14:49:23 +0000

    Footnote add

---
+++ modules/html-storybook-new/src/stories/Footnote.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Footnote } from '@reykjavik/hanna-react/Footnote';
+import { Meta, StoryObj } from '@storybook/react';
+
+import { HiddenTiger } from '../utils/HiddenTrigger.js';
+
+const meta: Meta<typeof Footnote> = {
+  title: 'components/Footnote',
+  component: Footnote,
+};
+export default meta;
+
+type Story = StoryObj<typeof Footnote>;
+
+const Component = () => {
+  return (
+    <>
+      <HiddenTiger>
+        <p>
+          This Atomic component can be placed inside/alongside just about any type of text
+          content.
+        </p>
+      </HiddenTiger>
+      <Footnote>
+        Please note that - <a href="">Bein útsending frá fundi borgarstjórnar</a> í
+        Ráðhúsi Reykjavíkur hefst kl. 14:00
+      </Footnote>
+    </>
+  );
+};
+
+export const _Footnote: Story = {
+  render: () => <Component />,
+};
