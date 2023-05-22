2e01cc2994d0faf284578e1f9bbd3982a4ae3be6
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-22 13:36:06 +0000

    Carousel add

---
+++ modules/html-storybook-new/src/stories/Carousel.stories.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Carousel } from '@reykjavik/hanna-react/Carousel';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Carousel> = {
+  title: 'components/Carousel',
+  component: Carousel,
+};
+export default meta;
+
+type Story = StoryObj<typeof Carousel>;
+
+const Component = () => {
+  return (
+    <>
+      <style>
+        {`
+        .Carousel p {
+          border: var(--border-default);
+          background-color: var(--color-suld-25);
+          padding: 1em;
+          margin-right: var(--grid-gutter);
+        }
+        .Carousel strong {
+          display: block;
+          width: max-content;
+        }
+      `}
+      </style>
+      <Carousel startSeen>
+        <p>
+          <strong>one</strong> Subtext
+        </p>
+        <p>
+          <strong>two</strong> Yöva gäta stahrt sömwaer. Ut enim ad minim veniam, letsi
+          Björk ifder svensk og latin makinen dehr graek.
+        </p>
+        <p>
+          <strong>three</strong> Ut enim ad minim veniam, letsi Björk ifder svensk og
+          latin makinen dehr graek
+        </p>
+        <p>
+          <strong>four</strong> Chockolate moose!
+        </p>
+      </Carousel>
+    </>
+  );
+};
+
+export const _Carousel: Story = {
+  render: () => <Component />,
+};
