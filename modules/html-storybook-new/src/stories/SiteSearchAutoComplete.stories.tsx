ac9a37d8940cd933ea53ef66b975ebfd57f522dc
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-23 12:35:29 +0000

    SiteSearchAutocomplete add

---
+++ modules/html-storybook-new/src/stories/SiteSearchAutoComplete.stories.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import range from '@hugsmidjan/qj/range';
+import { SiteSearchAutocomplete } from '@reykjavik/hanna-react/SiteSearchAutocomplete';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof SiteSearchAutocomplete> = {
+  title: 'components/Forms/Site Search Autocomplete',
+  component: SiteSearchAutocomplete,
+};
+export default meta;
+
+type Story = StoryObj<typeof SiteSearchAutocomplete>;
+
+const items = range(1, 5).map((value) => `Suggestion ${value}`);
+
+const Component = () => {
+  const [suggestions, setSuggestions] = useState<Array<string>>([]);
+  return (
+    <SiteSearchAutocomplete
+      suggestions={suggestions}
+      renderSuggestion={(suggestion) => {
+        return suggestion;
+      }}
+      setSuggestions={setSuggestions}
+      getSuggestionValue={(value) => value}
+      onSuggestionsFetchRequested={
+        () => setSuggestions(items)
+        // ({ value }) => setSuggestions(getSuggestions(value))
+      }
+    />
+  );
+};
+
+export const _SiteSearchAutocomplete: Story = {
+  render: () => <Component />,
+};
