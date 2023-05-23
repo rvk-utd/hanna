96ac88f94ed60d066cafc39489e92f1890c65d4d
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-23 09:35:48 +0000

    SearchResults add

---
+++ modules/html-storybook-new/src/stories/RelatedLinks.stories.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { CenterColumn } from '@reykjavik/hanna-react/CenterColumn';
+import { RelatedLinkItem, RelatedLinks } from '@reykjavik/hanna-react/RelatedLinks';
+import { boolean } from '@storybook/addon-knobs';
+import { Meta, StoryObj } from '@storybook/react';
+
+import { HiddenTiger } from '../utils/HiddenTrigger.js';
+
+const meta: Meta<typeof RelatedLinks> = {
+  title: 'components/RelatedLinks',
+  component: RelatedLinks,
+};
+export default meta;
+
+type Story = StoryObj<typeof RelatedLinks>;
+
+const TITLE = 'Tengt efni';
+
+const LINKS: Array<RelatedLinkItem> = [
+  {
+    href: 'https://abendingar.reykjavik.is',
+    label: 'Ábendingavefur',
+    type: 'external',
+  },
+  {
+    href: 'https://abendingar.reykjavik.is',
+    label: 'Ábendingavefur',
+    target: '_blank',
+  },
+  {
+    href: '/files/somepdfFile',
+    label: 'Eitthvað PDF skjal',
+    type: 'pdf',
+  },
+  {
+    href: '/files/some.pdf',
+    label: 'Eitthvað PDF skjal',
+  },
+  {
+    href: '/files/someOtherDocument',
+    label: 'Annars konar Skjal með afar langan titil sem brotnar milli lína',
+    type: 'document',
+  },
+  {
+    href: '/normal/link',
+    label: 'Vefsíða með tengdu efni',
+  },
+];
+
+const Component = () => {
+  const title = boolean('Title', true) ? TITLE : undefined;
+  const children = <RelatedLinks title={title} links={LINKS} />;
+
+  return (
+    <HiddenTiger
+      serverSide={children}
+      clientSide={<CenterColumn>{children}</CenterColumn>}
+    />
+  );
+};
+
+export const _RelatedLinks: Story = {
+  render: () => <Component />,
+};
---
+++ modules/html-storybook-new/src/stories/SearchResults.stories.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import range from '@hugsmidjan/qj/range';
+import {
+  SearchResults,
+  SearchResultsItemProps,
+} from '@reykjavik/hanna-react/SearchResults';
+import { getIllustrationUrl } from '@reykjavik/hanna-utils/assets';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof SearchResults> = {
+  title: 'components/SearchResults',
+  component: SearchResults,
+};
+export default meta;
+
+type Story = StoryObj<typeof SearchResults>;
+
+// ===========================================================================
+
+const items = range(1, 3).map(
+  (item): SearchResultsItemProps => ({
+    title: 'Niðurstaða ' + item,
+    meta: item % 3 === 1 ? ['Fréttir', '15. janúar 2020'] : undefined,
+    summary:
+      'Á leikskólum er haft að leiðarljósi að börnin njóti bernsku sinnar og læri og þroskist í leik og samveru.',
+    href: '/',
+  })
+);
+
+items.unshift({
+  highlight: true,
+  href: '',
+  title: 'Dagforeldrar í Reykjavík',
+  summary:
+    'Á leikskólum er haft að leiðarljósi að börnin njóti bernsku sinnar og læri og þroskist í leik og samveru.',
+  image: getIllustrationUrl('borgarstjori1'),
+  meta: '23. janúar 2021',
+});
+
+export const _SearchResults: Story = {
+  render: () => (
+    <SearchResults
+      status="results"
+      pageSize={10}
+      hits={items.length}
+      query="leitarorð"
+      items={items}
+    />
+  ),
+};
