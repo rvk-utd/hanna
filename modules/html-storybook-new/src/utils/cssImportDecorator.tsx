6c1b5b1b2434a419e960796b197c85256430e572
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-23 13:45:10 +0000

    console.log remove

--- modules/html-storybook-new/src/utils/cssImportDecorator.tsx
+++ modules/html-storybook-new/src/utils/cssImportDecorator.tsx
@@ -6,12 +6,9 @@ export const cssImportDecorator = (
   story: () => React.ReactNode,
   context: StoryContext
 ) => {
-  console.log('story: ', story);
-  console.log('context: ', context);
   const componentName = context.name;
   const cssTokens = `-basics,Layout,${componentName}`;
   const cssUrl = getCssBundleUrl(cssTokens);
-  console.log('cssUrl: ', cssUrl);
   return (
     <>
       <link rel="stylesheet" href={cssUrl} />
