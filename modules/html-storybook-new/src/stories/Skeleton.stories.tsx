f671edb771575857b6f3f0123fe03274c3729063
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-23 10:05:11 +0000

    Skeleton

---
+++ modules/html-storybook-new/src/stories/Skeleton.stories.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Skeleton } from '@reykjavik/hanna-react/Skeleton';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta = {
+  title: 'components/Skeleton',
+};
+export default meta;
+
+type Story = StoryObj;
+
+const Component = () => {
+  return (
+    <>
+      <p>
+        <br /> Single line:
+        <Skeleton text />
+      </p>
+      <p>
+        <br /> Multi-line:
+        <Skeleton text height={4} />
+      </p>
+      <p>
+        <br /> Block:
+        <Skeleton height={6} />
+      </p>
+
+      <p>
+        <br /> Multi item (default gap: 3):
+        <Skeleton items={3} height={3} text />
+      </p>
+      <p>
+        <br /> Multi block:
+        <Skeleton items={3} height={3} gap={1} />
+      </p>
+    </>
+  );
+};
+
+export const _Skeleton: Story = {
+  render: () => <Component />,
+};
