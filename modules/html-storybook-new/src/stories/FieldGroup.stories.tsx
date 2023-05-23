4169ad219821e3cf9f232eed1d455e3791f4301d
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-23 11:32:21 +0000

    FieldGroup add

---
+++ modules/html-storybook-new/src/stories/FieldGroup.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FieldGroup } from '@reykjavik/hanna-react/FieldGroup';
+import { boolean } from '@storybook/addon-knobs';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof FieldGroup> = {
+  title: 'components/Forms/FieldGroup',
+  component: FieldGroup,
+};
+export default meta;
+
+type Story = StoryObj<typeof FieldGroup>;
+
+const Component = () => {
+  const small = boolean('Small Legend', false);
+  const disabled = boolean('Disabled', false);
+  return (
+    <FieldGroup legend="Group headline" small={small} disabled={disabled}>
+      <p>...FormFields and other content goes here...</p>
+    </FieldGroup>
+  );
+};
+
+export const _FieldGroup: Story = {
+  render: () => <Component />,
+};
