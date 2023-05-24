d192ffcd617802268ef4edb3a766f8eabdeb4404
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-05-24 09:51:02 +0000

    layoutDecorator added

--- modules/html-storybook-new/src/utils/decorators.tsx
+++ modules/html-storybook-new/src/utils/decorators.tsx
@@ -1,6 +1,6 @@
 import type { Decorator } from '@storybook/react';
 
 import { cssImportDecorator } from './cssImportDecorator.js';
-// import { layoutDecorator } from './layoutDecorator.js';
+import { layoutDecorator } from './layoutDecorator.js';
 
-export const decorators: Array<Decorator> = [cssImportDecorator];
+export const decorators: Array<Decorator> = [cssImportDecorator, layoutDecorator];
--- modules/html-storybook-new/src/utils/layoutDecorator.tsx
+++ modules/html-storybook-new/src/utils/layoutDecorator.tsx
@@ -1,8 +1,90 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import getScrollbarWidth from '@hugsmidjan/qj/getScrollbarWidth';
+import getBemClass from '@hugsmidjan/react/utils/getBemClass';
+import { themeOptions } from '@reykjavik/hanna-react/constants';
+import { Layout } from '@reykjavik/hanna-react/Layout';
+import { select } from '@storybook/addon-knobs';
 import type { StoryContext } from '@storybook/react';
 
+import { StoryParameters } from './storytypes.js';
+
+const hide = (selector: string) => {
+  const elm = document.querySelector<HTMLElement>(selector);
+  elm && (elm.hidden = true);
+};
+
 export const layoutDecorator = (story: () => React.ReactNode, context: StoryContext) => {
-  console.log('context: ', context);
-  const componentName = context.name;
-  return <>{story()}</>;
+  const params = context.parameters as StoryParameters;
+  const layout = params.layout || {};
+  const modifier = layout.modifier;
+  const initialTheme = layout.theme || themeOptions[0];
+  const colorTheme = params.knobs?.theming
+    ? select('Theme', themeOptions, initialTheme)
+    : initialTheme;
+  const pos = layout.pos;
+  const fullLayout = layout.head != null || !!pos;
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(
+    () => {
+      document.documentElement.dataset.gridOverlay = 'true';
+      if (fullLayout) {
+        if (pos !== 'footer') {
+          hide('.Layout__footer');
+        }
+        if (pos !== 'nav') {
+          hide('.Layout__nav');
+        }
+        if (!layout.head) {
+          hide('.Layout__header');
+        }
+        if (pos === 'nav' || pos === 'footer') {
+          hide('.Layout__main');
+        }
+      } else {
+        getScrollbarWidth.setCSSvar();
+      }
+      const killLinks = (e: MouseEvent) => {
+        if ((e.target as HTMLElement | null)?.closest('a')) {
+          e.preventDefault();
+        }
+      };
+      document.addEventListener('click', killLinks);
+      return () => {
+        document.removeEventListener('click', killLinks);
+      };
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  const children = story();
+
+  return layout.disabled ? (
+    children
+  ) : fullLayout ? (
+    <Layout
+      modifier={modifier}
+      navChildren={pos === 'nav' && children}
+      footerChildren={pos === 'footer' && children}
+      colorTheme={colorTheme}
+    >
+      {pos !== 'nav' && pos !== 'footer' && children}
+    </Layout>
+  ) : (
+    <div
+      className={getBemClass('Layout', modifier)}
+      style={{ paddingTop: '0px' }}
+      data-color-theme={colorTheme}
+    >
+      <div className="Layout__content">
+        <div
+          className="Layout__main"
+          style={{ paddingTop: '30px', paddingBottom: '30px' }}
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
 };
