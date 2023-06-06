0c27496a52ff1001ed29a1fd7c9081166776e85e
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-06-06 09:49:35 +0000

    InfoHero controls config

--- modules/html-storybook-new/src/stories/InfoHero.stories.tsx
+++ modules/html-storybook-new/src/stories/InfoHero.stories.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { InfoHero, InfoHeroProps } from '@reykjavik/hanna-react/InfoHero';
 import { Sharpie } from '@reykjavik/hanna-react/Sharpie';
-import { boolean, optionsKnob } from '@storybook/addon-knobs';
 import { Meta, StoryObj } from '@storybook/react';
 
 import cityCouncilMemberImage from '../example_assets/Dagur-b.jpg';
 import landScapeImage from '../example_assets/Gallery--landscape--large.jpg';
 import swimmingpoolImage from '../example_assets/sundlaug.jpg';
+
+type InfoHeroImage = 'No image' | 'Portrait' | 'Landscape' | 'Pool';
+
+type InfoHeroControlProps = {
+  alignment: 'left' | 'right';
+  image: InfoHeroImage;
+  blingType: 'waves' | 'sunny-waves' | 'triangles' | 'circles';
+  swimmingPoolContent: boolean;
+  blurbText: boolean;
+  footerText: boolean;
+};
+type Story = StoryObj<InfoHeroControlProps>;
 
 const cityCouncilContent: InfoHeroProps = {
   title: 'Dagur B. Eggertsson',
@@ -31,69 +42,54 @@ const swimmingpoolContent: InfoHeroProps = {
   ),
 };
 
+const getImage = (image: InfoHeroImage) => {
+  if (image === 'Portrait') {
+    return cityCouncilMemberImage;
+  }
+  if (image === 'Landscape') {
+    return landScapeImage;
+  }
+  if (image === 'Pool') {
+    return swimmingpoolImage;
+  }
+  return '';
+};
+
 // ---------------------------------------------------------------------------
 
-const meta: Meta<typeof InfoHero> = {
+const meta: Meta<InfoHeroControlProps> = {
   title: 'InfoHero',
-  component: InfoHero,
 };
 export default meta;
 
-type Story = StoryObj<typeof InfoHero>;
+// type BlingOptions = 'waves' | 'sunny-waves' | 'triangles' | 'circles';
 
-const InfoHeroStory = () => {
-  const align = optionsKnob(
-    'Alignment',
-    {
-      Left: 'left',
-      Right: 'right',
-    },
-    'right',
-    { display: 'inline-radio' }
-  );
-  const image = optionsKnob(
-    'Image',
-    {
-      'No image': '',
-      Portrait: cityCouncilMemberImage,
-      Landscape: landScapeImage,
-      Pool: swimmingpoolImage,
-    },
-    cityCouncilMemberImage,
-    { display: 'inline-radio' }
-  );
-  const blingType = optionsKnob<InfoHeroProps['blingType']>(
-    'Bling type',
-    {
-      Waves: 'waves',
-      'Sunny Waves': 'sunny-waves',
-      Triangles: 'triangles',
-      Circles: 'circles',
-    },
-    'waves',
-    { display: 'inline-radio' }
-  );
+const InfoHeroStory: React.FC<InfoHeroControlProps> = ({
+  alignment,
+  image,
+  blingType,
+  swimmingPoolContent,
+  blurbText,
+  footerText,
+}) => {
+  const imageSrc = getImage(image);
 
-  const contentProps = boolean('Swimming Pool content', false)
-    ? swimmingpoolContent
-    : cityCouncilContent;
+  const contentProps = swimmingPoolContent ? swimmingpoolContent : cityCouncilContent;
 
-  const blurb = boolean('Blurb text', false) ? (
+  const blurb = blurbText ? (
     <>
       Lorem ipsum
       <br />
       Dolor sit
     </>
   ) : undefined;
-  const footer = boolean('Footer text', false)
-    ? 'Leebur deroor iehroom, minim chokolat moose.'
-    : undefined;
+  const footer = footerText ? 'Leebur deroor iehroom, minim chokolat moose.' : undefined;
   return (
     <InfoHero
-      key={align + (image !== '') + blingType}
+      key={alignment + (imageSrc !== '') + blingType}
       {...contentProps}
-      {...(image ? { image: { src: image } } : { image: undefined })}
-      align={align}
+      {...(imageSrc ? { image: { src: imageSrc } } : { image: undefined })}
+      align={alignment}
       buttons={[
         { href: '', label: 'Fjárhagslegir hagsmunir' },
         { href: '', label: 'Dagur á Facebook' },
@@ -110,5 +106,42 @@ const InfoHeroStory = () => {
 };
 
 export const _InfoHero: Story = {
-  render: () => <InfoHeroStory />,
+  render: (args: InfoHeroControlProps) => <InfoHeroStory {...args} />,
+  argTypes: {
+    alignment: {
+      control: 'inline-radio',
+      options: ['left', 'right'],
+      name: 'Alignment',
+    },
+    image: {
+      control: 'inline-radio',
+      options: ['No image', 'Portrait', 'Landscape', 'Pool'],
+      name: 'Image',
+    },
+    blingType: {
+      control: 'inline-radio',
+      options: ['waves', 'sunny-waves', 'triangles', 'circles'],
+      name: 'Bling type',
+    },
+    swimmingPoolContent: {
+      control: 'boolean',
+      name: 'Swimming Pool content',
+    },
+    blurbText: {
+      control: 'boolean',
+      name: 'Blurb text',
+    },
+    footerText: {
+      control: 'boolean',
+      name: 'Footer text',
+    },
+  },
+  args: {
+    alignment: 'right',
+    image: 'Portrait',
+    blingType: 'waves',
+    swimmingPoolContent: false,
+    blurbText: false,
+    footerText: false,
+  },
 };
