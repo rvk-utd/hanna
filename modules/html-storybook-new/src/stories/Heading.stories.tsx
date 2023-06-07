6ff18ac0fe472cddcca8d19b377e4946b5ccf2f1
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-06-07 10:07:10 +0000

    Heading controls

--- modules/html-storybook-new/src/stories/Heading.stories.tsx
+++ modules/html-storybook-new/src/stories/Heading.stories.tsx
@@ -1,45 +1,31 @@
 import React from 'react';
 import { Heading } from '@reykjavik/hanna-react/Heading';
-import { optionsKnob } from '@storybook/addon-knobs';
 import { Meta, StoryObj } from '@storybook/react';
 
-const meta: Meta<typeof Heading> = {
+type HeadingControlProps = {
+  layout: 'left' | 'right' | 'wide';
+  sizeVariant: 'normal' | 'small' | 'large';
+  headingLevel: 'h2' | 'h3';
+};
+
+const meta: Meta<HeadingControlProps> = {
   title: 'text/Heading',
-  component: Heading,
 };
 export default meta;
 
-type Story = StoryObj<typeof Heading>;
+type Story = StoryObj<HeadingControlProps>;
 
-const HeadingStory = () => {
-  const layout =
-    optionsKnob(
-      'Layout',
-      {
-        Left: '',
-        'Right aligned': 'right',
-        Wide: 'wide',
-      },
-      '',
-      { display: 'inline-radio' }
-    ) || undefined;
-  const size =
-    optionsKnob(
-      'Size variant',
-      {
-        'Normal (medium)': '',
-        Small: 'small',
-        Large: 'large',
-      },
-      '',
-      { display: 'inline-radio' }
-    ) || undefined;
-  const htmlTag =
-    optionsKnob('Heading level', { 'H2 (default)': '', H3: 'h3' }, '', {
-      display: 'inline-radio',
-    }) || undefined;
+const HeadingStory: React.FC<HeadingControlProps> = ({
+  layout,
+  sizeVariant,
+  headingLevel,
+}) => {
+  const _layout = layout !== 'left' ? layout : undefined;
+  const size = sizeVariant !== 'normal' ? sizeVariant : undefined;
 
-  const layoutProps = layout === 'wide' ? { wide: true } : { align: layout };
+  const htmlTag = headingLevel !== 'h2' ? headingLevel : undefined;
+
+  const layoutProps = _layout === 'wide' ? { wide: true } : { align: _layout };
   return (
     <Heading {...layoutProps} size={size} Tag={htmlTag}>
       Heading title
@@ -48,5 +34,47 @@ const HeadingStory = () => {
 };
 
 export const _Heading: Story = {
-  render: () => <HeadingStory />,
+  render: (args: HeadingControlProps) => <HeadingStory {...args} />,
+  argTypes: {
+    layout: {
+      control: {
+        type: 'inline-radio',
+        labels: {
+          left: 'Left',
+          right: 'Right aligned',
+          wide: 'Wide',
+        },
+      },
+      options: ['left', 'right', 'wide'],
+      name: 'Layout',
+    },
+    sizeVariant: {
+      control: {
+        type: 'inline-radio',
+        labels: {
+          normal: 'Normal (medium)',
+          small: 'Small',
+          large: 'Large',
+        },
+      },
+      options: ['normal', 'small', 'large'],
+      name: 'Size variant',
+    },
+    headingLevel: {
+      control: {
+        type: 'inline-radio',
+        labels: {
+          h2: 'H2 (default)',
+          h3: 'H3',
+        },
+      },
+      options: ['h2', 'h3'],
+      name: 'Heading level',
+    },
+  },
+  args: {
+    layout: 'left',
+    sizeVariant: 'normal',
+    headingLevel: 'h2',
+  },
 };
