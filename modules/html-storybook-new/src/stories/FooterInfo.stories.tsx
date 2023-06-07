3ec85ff08c9264d4691e42a578c0b297cad1ae1e
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-06-07 11:14:31 +0000

    FooterInfo controls

--- modules/html-storybook-new/src/stories/FooterInfo.stories.tsx
+++ modules/html-storybook-new/src/stories/FooterInfo.stories.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import FooterInfo from '@reykjavik/hanna-react/FooterInfo';
 import { Meta, StoryObj } from '@storybook/react';
 
 import { FooterInfoStory } from '../Shared/FooterInfo.js';
 
-const meta: Meta<typeof FooterInfo> = {
+const meta: Meta = {
   title: 'Layout/FooterInfo',
-  component: FooterInfo,
 };
 export default meta;
 
-type Story = StoryObj<typeof FooterInfo>;
+type Story = StoryObj;
 
 export const _FooterInfo: Story = {
   render: () => <FooterInfoStory />,
+  parameters: {
+    controls: { hideNoControlsWarning: true },
+  },
 };
