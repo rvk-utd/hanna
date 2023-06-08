879337967f7901141b3258d6cbfff57a8d4597c7
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-06-08 09:47:50 +0000

    Buttons controls

--- modules/html-storybook-new/src/stories/Buttons.stories.tsx
+++ modules/html-storybook-new/src/stories/Buttons.stories.tsx
@@ -3,41 +3,33 @@ import { ButtonPrimary } from '@reykjavik/hanna-react/ButtonPrimary';
 import { ButtonSecondary } from '@reykjavik/hanna-react/ButtonSecondary';
 import { ButtonTertiary } from '@reykjavik/hanna-react/ButtonTertiary';
 import { ObjectEntries } from '@reykjavik/hanna-utils';
-import { optionsKnob } from '@storybook/addon-knobs';
 import { Meta, StoryObj } from '@storybook/react';
+
+const sizeOptions = ['normal', 'small', 'wide'] as const;
+type Size = (typeof sizeOptions)[number];
+
+const variantOptions = ['normal', 'destructive'] as const;
+type Variant = (typeof variantOptions)[number];
+
+type ButtonsControlProps = {
+  size: Size;
+  variant: Variant;
+};
+
+type Story = StoryObj<ButtonsControlProps>;
 
 const meta: Meta = {
   title: 'buttons/Buttons',
 };
 export default meta;
 
-type Story = StoryObj;
+const ButtonsStory: React.FC<ButtonsControlProps> = ({ size, variant }) => {
+  const _size = size !== 'normal' ? size : '';
 
-const ButtonsStory = () => {
-  const size = optionsKnob(
-    'Size',
-    {
-      Normal: '',
-      'Small size': 'small',
-      'Wide variant': 'wide',
-    },
-    '',
-    { display: 'inline-radio' }
-  );
+  const _variant = variant === 'destructive' ? variant : undefined;
 
-  const variant =
-    optionsKnob(
-      'Variant',
-      {
-        Normal: '',
-        Destructive: 'destructive',
-      },
-      '',
-      { display: 'inline-radio' }
-    ) || undefined;
-
-  const variantName = variant === 'destructive' ? 'Destructive' : '';
-  const sizeName = size === 'wide' ? 'Wide' : size === 'small' ? 'Small' : '';
+  const variantName = _variant === 'destructive' ? 'Destructive' : '';
+  const sizeName = _size === 'wide' ? 'Wide' : _size === 'small' ? 'Small' : '';
   return (
     <>
       {ObjectEntries({
@@ -50,29 +42,29 @@ const ButtonsStory = () => {
           '': undefined,
           small: 'small',
           wide: supportsWide ? 'wide' : undefined,
-        }[size] as any;
+        }[_size] as any;
 
         return (
           <Fragment key={i}>
             <p>
-              <ButtonComponent size={sizeValue} variant={variant}>
+              <ButtonComponent size={sizeValue} variant={_variant}>
                 {sizeName} {variantName} {name} Button
               </ButtonComponent>{' '}
-              <ButtonComponent size={sizeValue} variant={variant} disabled>
+              <ButtonComponent size={sizeValue} variant={_variant} disabled>
                 Disabled
               </ButtonComponent>{' '}
-              <ButtonComponent size={sizeValue} variant={variant} aria-pressed="true">
+              <ButtonComponent size={sizeValue} variant={_variant} aria-pressed="true">
                 Pressed
               </ButtonComponent>
             </p>
             <p>
-              <ButtonComponent size={sizeValue} variant={variant} href="">
+              <ButtonComponent size={sizeValue} variant={_variant} href="">
                 {sizeName} {variantName} {name} Link
               </ButtonComponent>{' '}
             </p>
             <p>
               {' '}
-              <ButtonComponent size={sizeValue} variant={variant} icon="go-back">
+              <ButtonComponent size={sizeValue} variant={_variant} icon="go-back">
                 Go Back
               </ButtonComponent>{' '}
               {/** /}
@@ -89,7 +81,7 @@ const ButtonsStory = () => {
               {ButtonComponent !== ButtonTertiary && (
                 <ButtonComponent
                   size={sizeValue}
-                  variant={variant}
+                  variant={_variant}
                   icon={'go-forward' as 'none'}
                 >
                   Go Forward
@@ -123,7 +115,36 @@ const ButtonsStory = () => {
 };
 
 export const _Buttons: Story = {
-  render: () => <ButtonsStory />,
+  render: (args: ButtonsControlProps) => <ButtonsStory {...args} />,
+  argTypes: {
+    size: {
+      control: {
+        type: 'inline-radio',
+        labels: {
+          normal: 'Normal',
+          small: 'Small size',
+          wide: 'Wide variant',
+        },
+      },
+      options: sizeOptions,
+      name: 'Size',
+    },
+    variant: {
+      control: {
+        type: 'inline-radio',
+        labels: {
+          normal: 'Normal',
+          destructive: 'Destructive',
+        },
+      },
+      options: variantOptions,
+      name: 'Variant',
+    },
+  },
+  args: {
+    size: 'normal',
+    variant: 'normal',
+  },
   parameters: {
     css: { tokens: 'ButtonPrimary,ButtonSecondary,ButtonTertiary' },
   },
