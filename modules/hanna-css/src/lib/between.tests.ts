8c591dbf0a2f5c3329f39285dcbec3461b35d522
Már Örlygsson <mar.nospam@anomy.net>
2023-07-27 10:58:09 +0000

    tests: Add more tests for `between`

--- modules/hanna-css/src/lib/between.tests.ts
+++ modules/hanna-css/src/lib/between.tests.ts
@@ -1,3 +1,4 @@
+import { pct, px } from 'es-in-css';
 import o from 'ospec';
 
 import { between } from './between.js';
@@ -13,11 +14,28 @@ o.spec('between helper', () => {
     )('supports vw');
 
     o(between(16, 16, 320, 1368, '%')).equals('16px')(
-      'returns bare intercept when slopeFactor is 0'
+      'returns bare intercept when slopeFactor is zero'
     );
 
     o(between(16, 64, 320, 1280, 'vh')).equals('5vh')(
-      'returns bare slope when intercept is 0'
+      'returns bare slope when intercept is zero'
     );
+
+    // ----
+
+    o(between(px(16), 24, 320, 1368, '%')).equals(
+      'calc(0.7633587786259541% + 13.557251908396948px)'
+    )('accepts `from` as px');
+    o(between(16, px(24), 320, 1368, '%')).equals(
+      'calc(0.7633587786259541% + 13.557251908396948px)'
+    )('accepts `to` as px');
+    o(between(16, 24, px(320), 1368, '%')).equals(
+      'calc(0.7633587786259541% + 13.557251908396948px)'
+    )('accepts `min` as px');
+    o(between(16, 24, 320, px(1368), '%')).equals(
+      'calc(0.7633587786259541% + 13.557251908396948px)'
+    )('accepts `max` as px');
+    o(between(pct(5), 64, 320, 1280, 'vh')).equals('5vh')('accepts `from` as %');
+    o(between(16, pct(5), 320, 1280, 'vh')).equals('5vh')('accepts `to` as %');
   });
 });
