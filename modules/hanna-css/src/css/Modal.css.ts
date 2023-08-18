0c3927dd4259604d2229a6b4f2751a172a93686b
Már Örlygsson <mar.nospam@anomy.net>
2023-08-18 11:55:40 +0000

    fix: Add stricter scope to Modal's transition property

--- modules/hanna-css/src/css/Modal.css.ts
+++ modules/hanna-css/src/css/Modal.css.ts
@@ -60,6 +60,7 @@ export default css`
       box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
 
       transition: all 200ms ease-in;
+      transition-property: opacity, visibility, transform;
       transform: translateY(0);
       opacity: 1;
     }
