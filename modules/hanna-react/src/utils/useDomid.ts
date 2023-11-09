7672a322d5b5e22524226f80a87fb35a9a5b8e36
Már Örlygsson <mar.nospam@anomy.net>
2023-11-09 13:14:15 +0000

    docs: Improve JSDoc for useDomid

--- modules/hanna-react/src/utils/useDomid.ts
+++ modules/hanna-react/src/utils/useDomid.ts
@@ -7,11 +7,13 @@ const useId: undefined | (() => string) = React.useId;
 /**
  * Returns a stable, unique ID string.
  *
+ * Accepts an optional static ID string, which takes precedence.
+ *
  * Uses useId from React (v18+) when available, falling back on a custom
  * unique id generator.
  *
  * (NOTE: The custom generator causes angry hydration warnings in dev
- * mode and there's nothing we can do about it.)
+ * mode and there's nothing we can do about it, except to ignore them.)
  */
 export const useDomid = useId
   ? (staticId?: string): string => {
