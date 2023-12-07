06cb57b4e5fe1850a89ab034afef1c6000d97f89
Már Örlygsson <mar.nospam@anomy.net>
2023-12-07 09:10:19 +0000

    style: Remove never-used, temporary CSS from `Pagination` module

--- modules/hanna-css/src/css/Pagination.css.ts
+++ modules/hanna-css/src/css/Pagination.css.ts
@@ -4,13 +4,9 @@ import { mq } from '../lib/breakpoints.js';
 import { hannaVarOverride, hannaVars as vars } from '../lib/hannavars.js';
 import { iconStyle } from '../lib/icons.js';
 
-import { ButtonStyle, ButtonVariables } from './styles/buttons.js';
 import { LinkStyle_Reset } from './styles/links.js';
 import { hideText_css } from './utils/hideText.js';
 import { prem } from './utils/miscUtils.js';
-
-const _colorWhite = vars.color_white;
-const _colorBlack = vars.color_blackish;
 
 export default css`
   .Pagination {
@@ -96,53 +92,5 @@ export default css`
   }
   .Pagination__button--next::before {
     content: ${vars.icon__chevron_right};
-  }
-
-  /* ======================================================================== */
-
-  .PaginationOld {
-    display: flex;
-    align-items: center;
-    list-style: none;
-    grid-gap: 5px;
-  }
-  .PaginationOld__button {
-    ${ButtonStyle};
-    ${ButtonVariables.override({
-      backgroundColor: _colorWhite,
-      textColor: _colorBlack,
-      height: vars.space_4,
-    })}
-    padding: 0 15px;
-    min-width: auto;
-    margin: 0;
-  }
-  .PaginationOld__button:not([disabled], .PaginationOld__button--active):hover {
-    background-color: ${vars.color_suld_50};
-    box-shadow: none;
-  }
-  .PaginationOld__button:disabled:active {
-    background-color: ${_colorWhite};
-    color: ${_colorBlack};
-  }
-  .PaginationOld__button--active {
-    background-color: ${vars.color_faxafloi_100};
-    color: ${_colorWhite};
-  }
-  .PaginationOld__button--active:focus-visible,
-  .PaginationOld__button--active:hover {
-    color: ${_colorWhite};
-  }
-  .PaginationOld__button--back {
-    &::before {
-      ${iconStyle(vars.icon__chevron_left)}
-      font-size: 12px;
-    }
-  }
-  .PaginationOld__button--forward {
-    &::before {
-      ${iconStyle(vars.icon__chevron_right)}
-      font-size: 12px;
-    }
   }
 `;
