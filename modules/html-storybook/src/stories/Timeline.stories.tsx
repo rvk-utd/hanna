dcc23045cfa5d3a7f578cd49e748882b71b191ef
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2026-03-03 14:45:45 +0000

    storybook update

--- modules/html-storybook/src/stories/Timeline.stories.tsx
+++ modules/html-storybook/src/stories/Timeline.stories.tsx
@@ -10,6 +10,44 @@ type ControlProps = {
   state: (typeof stateOptions)[number];
 };
 
+type TimelineItem = {
+  title: string;
+  category: string;
+  description?: string;
+};
+
+const timelineItems: Array<TimelineItem> = [
+  {
+    title: 'Vantar upplýsingar',
+    category: 'Nafn starfsmanns',
+    description: 'Þarf að sanna sér deili',
+  },
+  {
+    title: 'Í vinnslu',
+    category: 'Nafn starfsmanns',
+  },
+  {
+    title: 'Vantar upplýsingar',
+    category: 'Nafn starfsmanns',
+    description: 'Vantar sakavottorð',
+  },
+  {
+    title: 'Í vinnslu',
+    category: 'Nafn starfsmanns',
+  },
+  {
+    title: 'Póstur sendur',
+    category: 'Nafn starfsmanns',
+    description: 'Áminning um að sinna einstaklingsáætlun',
+  },
+  {
+    title: 'Athugasemd',
+    category: 'Nafn starfsmanns',
+    description:
+      'Umsækjandi þarf að koma með skilríki þegar hann mætir. Þetta er nauðsynlegt til að staðfesta auðkenni hans. Vinsamlegast tryggið að skilríkin séu gild.',
+  },
+];
+
 // ---------------------------------------------------------------------------
 
 const meta: Meta<ControlProps> = {
@@ -23,34 +61,20 @@ export default meta;
 const TimelineStory = (props: ControlProps) => {
   const { type, state } = props;
 
-  return (
-    <Timeline
-      key="hugahuga"
-      items={[
-        {
-          title: 'Vantar upplýsingar',
-          date: new Date(2025, 1, 12),
-          category: 'Starfsmaður Erlingsson',
-          curent: true,
-          description: 'Þarf vantar *gerð skjals*',
-        },
-        {
-          title: 'Í vinnslu',
-          date: new Date(2025, 1, 10),
-          category: 'Starfsmaður Erlingsson',
-          curent: false,
-        },
-        'skeleton',
-        {
-          title: 'Ný umsókn',
-          date: new Date(2025, 0, 10),
-          category: 'Umsækjandi Umsækjandason',
-          curent: false,
-        },
-        'skeleton',
-      ]}
-    />
-  );
+  const nrOfItems = 6;
+  // Generate dynamic timeline items
+  const items = Array.from({ length: nrOfItems }, (_, i) => {
+    const timelineItem = timelineItems[i];
+    return {
+      title: timelineItem?.title,
+      date: new Date(2025, 0, 10 - i * 2),
+      category: timelineItem?.category,
+      curent: i === 0,
+      description: timelineItem?.description,
+    };
+  });
+
+  return <Timeline key="hugahuga" items={items} />;
 };
 
 export const _Timeline: StoryObj<ControlProps> = {
