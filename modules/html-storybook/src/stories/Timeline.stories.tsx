b64deb6d28e317e779b2cd981fee552fd03e4098
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2026-03-24 12:54:37 +0000

    TimelineItems moved to dummydata

--- modules/html-storybook/src/stories/Timeline.stories.tsx
+++ modules/html-storybook/src/stories/Timeline.stories.tsx
@@ -1,46 +1,8 @@
 import React from 'react';
-import { Timeline, TimeLineItem } from '@reykjavik/hanna-react/Timeline';
+import { Timeline } from '@reykjavik/hanna-react/Timeline';
 import { Meta, StoryObj } from '@storybook/react';
 
-import { lorem } from '../utils/_dummyData.js';
-
-export const timelineItems: Array<TimeLineItem> = [
-  {
-    title: 'Vantar upplýsingar',
-    category: 'Nafn starfsmanns',
-    description: 'Þarf að sanna sér deili',
-    date: new Date(2025, 0, 10),
-  },
-  {
-    title: 'Í vinnslu',
-    category: 'Nafn starfsmanns',
-    date: new Date(2025, 0, 8),
-    status: { label: 'Móttekið', color: 'green' },
-  },
-  {
-    title: 'Vantar upplýsingar',
-    category: 'Nafn starfsmanns',
-    description: 'Vantar sakavottorð',
-    date: new Date(2025, 0, 6),
-  },
-  {
-    title: 'Athugasemd',
-    category: 'Nafn starfsmanns',
-    description: lorem.medium,
-    date: new Date(2025, 0, 4),
-  },
-  {
-    title: 'Í vinnslu',
-    category: 'Nafn starfsmanns',
-    date: new Date(2025, 0, 2),
-  },
-  {
-    title: 'Póstur sendur',
-    category: 'Nafn starfsmanns',
-    description: 'Áminning um að sinna einstaklingsáætlun',
-    date: new Date(2024, 11, 31),
-  },
-];
+import { timelineItems } from '../utils/_dummyData.js';
 
 type ControlProps = {
   loadingMore: boolean;
