1448e2adb02d1e591ff20cb3d845913e3a43ade1
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2026-03-24 13:03:35 +0000

    timeline exported items removed

--- modules/visual-tests/src/routes/test/Timeline.tsx
+++ modules/visual-tests/src/routes/test/Timeline.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import type { V2_MetaFunction } from '@remix-run/node';
 import { Timeline } from '@reykjavik/hanna-react/Timeline';
-import { timelineItems } from 'modules/html-storybook/src/stories/Timeline.stories';
 
 import { Minimal } from '../../layout/Minimal.js';
+import { lorem } from '../../test-helpers/dummyData.js';
 import type { TestingInfo } from '../../test-helpers/testingInfo.js';
 import { autoTitle } from '../../utils/meta.js';
-
-const _timelineItems = [...timelineItems, 'loading'];
 
 // ---------------------------------------------------------------------------
 
@@ -20,7 +18,34 @@ export default function () {
   return (
     <Minimal>
       <div>
-        <Timeline items={_timelineItems} />
+        <Timeline
+          items={[
+            {
+              title: 'Vantar upplýsingar',
+              category: 'Nafn starfsmanns',
+              description: 'Þarf að sanna sér deili',
+              date: new Date(2025, 0, 10),
+            },
+            {
+              title: 'Í vinnslu',
+              category: 'Nafn starfsmanns',
+              date: new Date(2025, 0, 8),
+              status: { label: 'Móttekið', color: 'green' },
+            },
+            {
+              title: 'Vantar upplýsingar',
+              category: 'Nafn starfsmanns',
+              description: 'Vantar sakavottorð',
+              date: new Date(2025, 0, 6),
+            },
+            {
+              title: 'Athugasemd',
+              category: 'Nafn starfsmanns',
+              description: lorem.medium,
+              date: new Date(2025, 0, 4),
+            },
+          ]}
+        />
       </div>
     </Minimal>
   );
