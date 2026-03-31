115f9884fc77361f63b7fcc57346d0169e6093bf
Már Örlygsson <mar.nospam@anomy.net>
2026-03-31 11:25:11 +0000

    tests: Add more component variations to the visual-tests

--- modules/visual-tests/src/routes/test/Timeline.tsx
+++ modules/visual-tests/src/routes/test/Timeline.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import type { V2_MetaFunction } from '@remix-run/node';
-import { Timeline } from '@reykjavik/hanna-react/Timeline';
+import { Timeline, TimelineProps } from '@reykjavik/hanna-react/Timeline';
 
+import { DummyBlock } from '../../layout/DummyBlock.js';
 import { Minimal } from '../../layout/Minimal.js';
 import { lorem } from '../../test-helpers/dummyData.js';
 import type { TestingInfo } from '../../test-helpers/testingInfo.js';
@@ -11,40 +12,44 @@ import { autoTitle } from '../../utils/meta.js';
 
 export const meta: V2_MetaFunction = autoTitle;
 
+const items: TimelineProps['items'] = [
+  {
+    title: 'Latest item',
+    category: 'User or author',
+    description: 'Some short summary or description',
+    date: new Date(2025, 1, 10),
+  },
+  {
+    title: 'This item is flagged as current, and has a weirdly long title',
+    category: 'Category',
+    status: { label: 'Móttekið', color: 'green' },
+    current: true,
+  },
+  {
+    title: 'Previous item',
+    description: lorem.medium,
+    date: new Date(2025, 0, 6),
+  },
+  {
+    title: 'First item',
+    description: 'Short description for this',
+    date: new Date(2024, 10, 30),
+  },
+];
+
+const twoItems = [items[0]!, items[items.length - 1]!];
+
 // // Use `handle` if you're using multiple Hanna compnents
 // export const handle = cssTokens('Token');
 
 export default function () {
   return (
     <Minimal>
-      <Timeline
-        items={[
-          {
-            title: 'Vantar upplýsingar',
-            category: 'Nafn starfsmanns',
-            description: 'Þarf að sanna sér deili',
-            date: new Date(2025, 0, 10),
-          },
-          {
-            title: 'Í vinnslu',
-            category: 'Nafn starfsmanns',
-            date: new Date(2025, 0, 8),
-            status: { label: 'Móttekið', color: 'green' },
-          },
-          {
-            title: 'Vantar upplýsingar',
-            category: 'Nafn starfsmanns',
-            description: 'Vantar sakavottorð',
-            date: new Date(2025, 0, 6),
-          },
-          {
-            title: 'Athugasemd',
-            category: 'Nafn starfsmanns',
-            description: lorem.medium,
-            date: new Date(2025, 0, 4),
-          },
-        ]}
-      />
+      <Timeline items={items} />
+      <DummyBlock thin />
+      <Timeline items={twoItems} hideTime title="With 'title' and time hidden" />
+      <DummyBlock thin />
+      <Timeline items={twoItems} oldestFirst />
     </Minimal>
   );
 }
