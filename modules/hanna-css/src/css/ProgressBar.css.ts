789ed2c93d3d1de623632b535238f58e3aeda57e
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2026-06-29 11:43:16 +0000

    border radius fix

--- modules/hanna-css/src/css/ProgressBar.css.ts
+++ modules/hanna-css/src/css/ProgressBar.css.ts
@@ -1,9 +1,10 @@
-import { css } from 'es-in-css';
+import { css, px } from 'es-in-css';
 
 import { hannaVars as vars } from '../lib/hannavars.js';
 
 const TRACK_COLOR = vars.color_faxafloi_100;
 const TRACK_BG_COLOR = vars.color_faxafloi_25;
+const BORDER_RADIUS = px(99);
 
 export default css`
   progress {
@@ -12,19 +13,24 @@ export default css`
     appearance: none;
     border: none;
     background-color: ${TRACK_BG_COLOR};
+    border-radius: ${BORDER_RADIUS};
   }
 
-  /* Track background - Chrome / Safari / Edge */
+  /* Track - Chrome / Safari / Edge */
   progress::-webkit-progress-bar {
     background-color: ${TRACK_BG_COLOR};
+    border-radius: ${BORDER_RADIUS};
+    border-radius: ${BORDER_RADIUS};
   }
 
   /* Track filled - Chrome / Safari / Edge */
   progress::-webkit-progress-value {
     background-color: ${TRACK_COLOR};
+    border-top-left-radius: ${BORDER_RADIUS};
+    border-bottom-left-radius: ${BORDER_RADIUS};
   }
 
-  /* Track filled - Firefox */
+  /* Track - Firefox */
   progress::-moz-progress-bar {
     background-color: ${TRACK_COLOR};
   }
