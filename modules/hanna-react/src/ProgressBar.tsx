df2536119ebbe09f80cd69b8eada46cf7fb94a66
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2026-06-29 10:41:30 +0000

    html init

---
+++ modules/hanna-react/src/ProgressBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const ProgressBar = () => {
+  return (
+    <div>
+      <p>huga buga</p>
+      <progress id="file" max="100" value="50">
+        70%
+      </progress>
+    </div>
+  );
+};
+
+export default ProgressBar;
---
+++ modules/html-storybook/src/stories/ProgressBar.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta = {
+  title: 'Progressbar',
+  parameters: {
+    controls: { hideNoControlsWarning: true },
+    viewport: { defaultViewport: 'responsive' },
+  },
+};
+
+export default meta;
+
+export const _ProgressBar: StoryObj = {
+  render: () => (
+    <div>
+      <p>huga buga</p>
+      <progress id="file" max="100" value="50">
+        70%
+      </progress>
+    </div>
+  ),
+};
