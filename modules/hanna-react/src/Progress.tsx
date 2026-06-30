3adaffaf50944f33605a83272c321a9c916271b5
Már Örlygsson <mar.nospam@anomy.net>
2026-06-30 10:15:43 +0000

    feat: Suggested "Progress" component combining spinner and bar modes

---
+++ modules/hanna-react/src/Progress.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { modifiedClass } from '@reykjavik/hanna-utils';
+
+export type ProgressProps = {
+  /** Render as a circular spinner instead of a line/bar */
+  spinner?: boolean;
+
+  /**
+   * The value of the progress bar from 0-100.
+   * If `undefined` the progress will show an "indeterminate" state.
+   */
+  percent?: number;
+
+  /** If the progress bar should be display a "done" state */
+  done?: boolean;
+
+  ariaLabel?: string;
+  ariaLabelledBy?: string;
+  id?: string;
+};
+
+export const Progress = (props: ProgressProps) => {
+  let { percent, done } = props;
+  if (done) {
+    percent = 100;
+  }
+  let bounds: { min?: number; max?: number } = {};
+  if (percent != null) {
+    bounds = { min: 0, max: 100 };
+    percent = Math.max(0, Math.min(100, Math.round(percent)));
+  }
+  return (
+    <span
+      className={modifiedClass('Progress', [props.spinner && 'spinner', done && 'done'])}
+      role="progressbar"
+      aria-valuemin={bounds.min}
+      aria-valuemax={bounds.max}
+      // NOTE: Progress is ndeterminate state is derived from this prop being absent
+      aria-valuenow={percent}
+      aria-label={props.ariaLabel || undefined}
+      aria-labelledby={props.ariaLabelledBy || undefined}
+      id={props.id}
+    >
+      {percent != null && <span className="Progress__value">{percent}%</span>}
+    </span>
+  );
+};
